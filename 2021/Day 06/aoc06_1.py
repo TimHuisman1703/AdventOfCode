87042e831ad327443ad02535347d0ac56ad967b0
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-06 06:43:44 +0100

    Day 6

--- 2021/Day 06/aoc06_1.py
+++ 2021/Day 06/aoc06_1.py
@@ -1,4 +1,16 @@
 file = open("aoc06_input.txt")
-l = file.read().split("\n")
+l = [int(j) for j in file.read().split(",")]
 file.close()
 
+N = 80
+
+for day in range(N):
+	to_add = 0
+	for i in range(len(l)):
+		l[i] -= 1
+		if l[i] == -1:
+			to_add += 1
+			l[i] = 6
+	l += [8] * to_add
+
+print(len(l))
--- 2021/Day 06/aoc06_2.py
+++ 2021/Day 06/aoc06_2.py
@@ -1,4 +1,27 @@
 file = open("aoc06_input.txt")
-l = file.read().split("\n")
+l = [int(j) for j in file.read().split(",")]
 file.close()
 
+N = 256
+
+mem = [[-1 for left in range(N + 1)] for timer in range(9)]
+
+def get_children(timer, left):
+	global mem
+
+	if left == 0:
+		return 1
+	
+	if mem[timer][left] > -1:
+		return mem[timer][left]
+	
+	result = 0
+	if timer > 0:
+		result = get_children(timer - 1, left - 1)
+	else:
+		result += get_children(6, left - 1) + get_children(8, left - 1)
+	
+	mem[timer][left] = result
+	return result
+
+print(sum(get_children(j, N) for j in l))
