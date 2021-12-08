6e1b749d16d59ba71310072d7dd1fe33977989d7
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-08 14:58:53 +0100

    Day 8

--- 2021/Day 08/aoc08_1.py
+++ 2021/Day 08/aoc08_1.py
@@ -2,3 +2,14 @@ file = open("aoc08_input.txt")
 l = file.read().split("\n")
 file.close()
 
+c = 0
+for i in l:
+	s, d = i.split(" | ")
+	s = s.split()
+	d = d.split()
+
+	for i in d:
+		if len(i) in [2, 3, 4, 7]:
+			c += 1
+
+print(c)
