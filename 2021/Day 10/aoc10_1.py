0042d58643b79c8ed72f6410e409be3dc5b8778f
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-10 20:48:04 +0100

    Day 10

--- 2021/Day 10/aoc10_1.py
+++ 2021/Day 10/aoc10_1.py
@@ -2,3 +2,18 @@ file = open("aoc10_input.txt")
 l = file.read().split("\n")
 file.close()
 
+cost = 0
+for s in l:
+	stack = []
+	
+	for c in s:
+		if c in "([{<":
+			stack += ["([{<".index(c)]
+		if c in ")]}>":
+			if stack and stack[-1] == ")]}>".index(c):
+				stack = stack[:-1]
+			else:
+				cost += [3, 57, 1197, 25137][")]}>".index(c)]
+				break
+
+print(cost)
--- 2021/Day 10/aoc10_2.py
+++ 2021/Day 10/aoc10_2.py
@@ -2,3 +2,24 @@ file = open("aoc10_input.txt")
 l = file.read().split("\n")
 file.close()
 
+scores = []
+
+for s in l:
+	stack = []
+	
+	for c in s:
+		if c in "([{<":
+			stack += ["([{<".index(c)]
+		if c in ")]}>":
+			if stack and stack[-1] == ")]}>".index(c):
+				stack = stack[:-1]
+			else:
+				break
+	else:
+		curr = 0
+		for c in stack[::-1]:
+			curr = curr * 5 + (c + 1)
+		scores += [curr]
+
+scores = sorted(scores)
+print(scores[len(scores) // 2])
