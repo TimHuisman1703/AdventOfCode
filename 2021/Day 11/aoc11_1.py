13beb07f63e9097c4a35da73e5eb9d73ad0624ed
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-11 13:22:14 +0100

    Day 11

--- 2021/Day 11/aoc11_1.py
+++ 2021/Day 11/aoc11_1.py
@@ -1,4 +1,31 @@
 file = open("aoc11_input.txt")
-l = file.read().split("\n")
+g = [[int(j) for j in row] for row in file.read().split("\n")]
 file.close()
 
+STEPS = 100
+
+c = 0
+
+for _ in range(STEPS):
+	changed = True
+	while changed:
+		changed = False
+
+		for iy in range(len(g)):
+			for ix in range(len(g[0])):
+				if g[iy][ix] >= 9:
+					for jy in range(max(0, iy-1), min(len(g), iy+2)):
+						for jx in range(max(0, ix-1), min(len(g[0]), ix+2)):
+							g[jy][jx] += 1
+					
+					changed = True
+					g[iy][ix] = -1000000000
+		
+	for iy in range(len(g)):
+		for ix in range(len(g[0])):
+			if g[iy][ix] < 0:
+				c += 1
+				g[iy][ix] = -1
+			g[iy][ix] += 1
+
+print(c)
--- 2021/Day 11/aoc11_2.py
+++ 2021/Day 11/aoc11_2.py
@@ -1,4 +1,33 @@
 file = open("aoc11_input.txt")
-l = file.read().split("\n")
+g = [[int(j) for j in row] for row in file.read().split("\n")]
 file.close()
 
+step = 1
+while 1:
+	changed = True
+	while changed:
+		changed = False
+
+		for iy in range(len(g)):
+			for ix in range(len(g[0])):
+				if g[iy][ix] >= 9:
+					for jy in range(max(0, iy-1), min(len(g), iy+2)):
+						for jx in range(max(0, ix-1), min(len(g[0]), ix+2)):
+							g[jy][jx] += 1
+					
+					changed = True
+					g[iy][ix] = -1000000000
+	
+	c = 0
+	for iy in range(len(g)):
+		for ix in range(len(g[0])):
+			if g[iy][ix] < 0:
+				c += 1
+				g[iy][ix] = -1
+			g[iy][ix] += 1
+	
+	if c == len(g) * len(g[0]):
+		print(step)
+		break
+
+	step += 1
