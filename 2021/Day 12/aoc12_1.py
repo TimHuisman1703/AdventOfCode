fe8922db92c351391c54086d20c2bd9001573e53
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-13 05:58:32 +0100

    Day 12

--- 2021/Day 12/aoc12_1.py
+++ 2021/Day 12/aoc12_1.py
@@ -2,3 +2,32 @@ file = open("aoc12_input.txt")
 l = file.read().split("\n")
 file.close()
 
+n = {}
+
+for i in l:
+	a, b = i.split("-")
+	
+	if a not in n.keys():
+		n[a] = []
+	if b not in n.keys():
+		n[b] = []
+	
+	n[a] += [b]
+	n[b] += [a]
+
+queue = ["start"]
+paths = set()
+
+while queue:
+	curr = queue.pop()
+	path = curr.split("-")
+
+	if path[-1] == "end":
+		paths.add(curr)
+		continue
+
+	for a in n[path[-1]]:
+		if a.isupper() or a not in path:
+			queue += [curr + "-" + a]
+
+print(len(paths))
--- 2021/Day 12/aoc12_2.py
+++ 2021/Day 12/aoc12_2.py
@@ -2,3 +2,34 @@ file = open("aoc12_input.txt")
 l = file.read().split("\n")
 file.close()
 
+n = {}
+
+for i in l:
+	a, b = i.split("-")
+	
+	if a not in n.keys():
+		n[a] = []
+	if b not in n.keys():
+		n[b] = []
+	
+	n[a] += [b]
+	n[b] += [a]
+
+queue = [("start", False)]
+paths = set()
+
+while queue:
+	curr, used = queue.pop()
+	path = curr.split("-")
+
+	if path[-1] == "end":
+		paths.add(curr)
+		continue
+
+	for a in n[path[-1]]:
+		if a.isupper() or a not in path:
+			queue += [(curr + "-" + a, used)]
+		elif not used and a != "start":
+			queue += [(curr + "-" + a, True)]
+
+print(len(paths))
