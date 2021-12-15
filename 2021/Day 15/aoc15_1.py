ed81af2316244377686abc860bb87af35aefa817
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-15 15:22:38 +0100

    Day 15

--- 2021/Day 15/aoc15_1.py
+++ 2021/Day 15/aoc15_1.py
@@ -2,3 +2,26 @@ file = open("aoc15_input.txt")
 l = file.read().split("\n")
 file.close()
 
+import heapq
+
+g = [[int(j) for j in row] for row in l]
+
+queue = [(0, 0, 0)]
+visited = set()
+
+while 1:
+	cost, x, y = heapq.heappop(queue)
+
+	if x == len(g[0]) - 1 and y == len(g) - 1:
+		print(cost)
+		break
+	
+	if (x, y) in visited:
+		continue
+	visited.add((x, y))
+
+	for ix, iy in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+		if ix < 0 or ix > len(g[0]) - 1 or iy < 0 or iy > len(g) - 1:
+			continue
+
+		heapq.heappush(queue, (cost + g[iy][ix], ix, iy))
