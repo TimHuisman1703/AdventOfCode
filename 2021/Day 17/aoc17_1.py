a16955c561e788acf913e4ac0625f0b7e286895e
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-17 08:47:48 +0100

    Day 17

--- 2021/Day 17/aoc17_1.py
+++ 2021/Day 17/aoc17_1.py
@@ -1,4 +1,8 @@
 file = open("aoc17_input.txt")
-l = file.read().split("\n")
+l = file.read().split()
 file.close()
 
+min_x, max_x = [int(j) for j in l[2][2:-1].split("..")]
+min_y, max_y = [int(j) for j in l[3][2:].split("..")]
+
+print(-min_y * (-min_y - 1) // 2)
--- 2021/Day 17/aoc17_2.py
+++ 2021/Day 17/aoc17_2.py
@@ -1,4 +1,36 @@
 file = open("aoc17_input.txt")
-l = file.read().split("\n")
+l = file.read().split()
 file.close()
 
+min_x, max_x = [int(j) for j in l[2][2:-1].split("..")]
+min_y, max_y = [int(j) for j in l[3][2:].split("..")]
+
+min_vx = 0
+while min_vx * (min_vx + 1) // 2 < min_x:
+	min_vx += 1
+max_vx = max_x
+
+min_vy = min_y
+max_vy = -min_y
+
+c = 0
+
+for i in range(min_vy, max_vy + 1):
+	for j in range(min_vx, max_vx + 1):
+		vx, vy = j, i
+		x, y = 0, 0
+		while x <= max_x and y >= min_y:
+			x += vx
+			y += vy
+
+			if vx < 0:
+				vx += 1
+			elif vx > 0:
+				vx -= 1
+			vy -= 1
+
+			if min_x <= x and x <= max_x and min_y <= y and y <= max_y:
+				c += 1
+				break
+
+print(c)
