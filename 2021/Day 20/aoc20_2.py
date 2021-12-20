becf24f7827fcf53d2ae54ea4b01ce9376e76ebc
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-20 11:11:11 +0100

    Day 20

--- 2021/Day 20/aoc20_2.py
+++ 2021/Day 20/aoc20_2.py
@@ -2,3 +2,42 @@ file = open("aoc20_input.txt")
 l = file.read().split("\n")
 file.close()
 
+STEPS = 50
+
+min_x, max_x, min_y, max_y = 10*100, -10*100, 10*100, -10*100
+
+a = l[0]
+s = set()
+for iy in range(len(l) - 2):
+	for ix in range(len(l[iy+2])):
+		if l[iy+2][ix] == "#":
+			s.add((ix, iy))
+			if ix < min_x:
+				min_x = ix
+			if ix + 1 > max_x:
+				max_x = ix + 1
+			if iy < min_y:
+				min_y = iy
+			if iy + 1 > max_y:
+				max_y = iy + 1
+
+for step in range(STEPS):
+	min_x, max_x, min_y, max_y = min_x - 1, max_x + 1, min_y - 1, max_y + 1
+
+	ns = set()
+	for iy in range(min_y, max_y):
+		for ix in range(min_x, max_x):
+			i = 0
+			for jy in range(iy - 1, iy + 2):
+				for jx in range(ix - 1, ix + 2):
+					i *= 2
+					if (jx, jy) in s or (step % 2 == 1 and (jy < min_y + 1 or jy > max_y - 2 or jx < min_x + 1 or jx > max_x - 2)):
+						i += 1
+			if a[i] == "#":
+				ns.add((ix, iy))
+	s = ns
+
+	if step % 10 == 9:
+		print(f"{step + 1}/{STEPS}")
+
+print(len(s))
