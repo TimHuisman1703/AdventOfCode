157883d74ac3575e887bdff1b0927a6c87aea81b
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-20 23:17:36 +0100

    Day 20 (2015)

--- 2015/Day 20/aoc20_1.py
+++ 2015/Day 20/aoc20_1.py
@@ -1,12 +1,47 @@
 file = open("aoc20_input.txt")
-l = [j for j in file.read().split("\n")]
+p = int(file.read())
 file.close()
 
-house = 1
-for house in range(1, 10001):
-	s = 0
-	for j in range(1, house+1):
-		if house % j == 0:
-			s += j
-	print(house, s)
-	house += 1
+primes = []
+
+def get_first_prime(n):
+	global primes
+
+	for i in primes:
+		if n % i == 0:
+			return i
+	
+	primes.append(n)
+	return n
+
+def prime_divide(b, i):
+	a = 1
+	while b % i == 0:
+		a *= i
+		b //= i
+	return a, b
+
+p //= 10
+d = {1: 1}
+m = 0
+
+i = 1
+while d[i] < p:
+	i += 1
+
+	div = get_first_prime(i)
+	if div == i:
+		d[i] = 1 + i
+	else:
+		a, b = prime_divide(i, div)
+		if b > 1:
+			d[i] = d[a] * d[b]
+		else:
+			d[i] = d[a // div] * div + 1
+	
+	if d[i] > m:
+		m = d[i]
+	if i % 10000 == 0:
+		print(i, d[i], m)
+
+print(i)
