e291e1922a20fd8b6d93fe6807d7d3185a1b8d78
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-21 09:22:52 +0100

    Day 21

--- 2021/Day 21/aoc21_1.py
+++ 2021/Day 21/aoc21_1.py
@@ -2,3 +2,20 @@ file = open("aoc21_input.txt")
 l = file.read().split("\n")
 file.close()
 
+x = int(l[0].split()[4])
+y = int(l[1].split()[4])
+
+a = b = 0
+
+steps = 0
+while a < 1000 and b < 1000:
+	steps += 1
+
+	if steps % 2 == 1:
+		x = (x + 9 * steps - 4) % 10 + 1
+		a += x
+	else:
+		y = (y + 9 * steps - 4) % 10 + 1
+		b += y
+
+print(3 * steps * min(a, b))
--- 2021/Day 21/aoc21_2.py
+++ 2021/Day 21/aoc21_2.py
@@ -2,3 +2,38 @@ file = open("aoc21_input.txt")
 l = file.read().split("\n")
 file.close()
 
+x = int(l[0].split()[4])
+y = int(l[1].split()[4])
+
+mem = {}
+def opt(a, b, x, y, turn):
+	key = (a, b, x, y, turn)
+	if key in mem.keys():
+		return mem[key]
+	
+	if a <= 0:
+		mem[key] = (1, 0)
+		return (1, 0)
+	if b <= 0:
+		mem[key] = (0, 1)
+		return (0, 1)
+
+	wa, wb = 0, 0
+	
+	for die_a in range(1, 4):
+		for die_b in range(1, 4):
+			for die_c in range(1, 4):
+				t = die_a + die_b + die_c
+				if turn == 0:
+					nx = (x - 1 + t) % 10 + 1
+					da, db = opt(a - nx, b, nx, y, 1 - turn)
+				else:
+					ny = (y - 1 + t) % 10 + 1
+					da, db = opt(a, b - ny, x, ny, 1 - turn)
+				wa, wb = wa + da, wb + db
+	
+	mem[key] = (wa, wb)
+	return mem[key]
+
+wa, wb = opt(21, 21, x, y, 0)
+print(max(wa, wb))
