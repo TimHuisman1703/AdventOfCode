d54a28c7035e61344454b9d69fdd55215f08a34f
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-21 19:08:54 +0100

    Day 25 (2015)

--- 2015/Day 25/aoc25_1.py
+++ 2015/Day 25/aoc25_1.py
@@ -1,4 +1,16 @@
 file = open("aoc25_input.txt")
-l = file.read().split("\n")
+l = file.read().split()
 file.close()
 
+y = int(l[15][:-1])
+x = int(l[17][:-1])
+
+a = x + y - 2
+b = a * (a + 1) // 2 + x - 1
+print(f"Buckle up, we're gonna iterate {b} times!")
+
+c = 20151125
+for i in range(b):
+	c = (c * 252533) % 33554393
+
+print(c)
