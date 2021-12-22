ed0d4cb3d3ab1a19527b04b3d0088c31f1c1973f
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-22 10:12:18 +0100

    Day 22

--- 2021/Day 22/aoc22_1.py
+++ 2021/Day 22/aoc22_1.py
@@ -2,3 +2,27 @@ file = open("aoc22_input.txt")
 l = file.read().split("\n")
 file.close()
 
+cubes = []
+
+for i in l[:20]:
+	on, a = i.split()
+	on = on == "on"
+	x, y, z = a.split(",")
+	min_x, max_x = [int(j) for j in x[2:].split("..")]
+	min_y, max_y = [int(j) for j in y[2:].split("..")]
+	min_z, max_z = [int(j) for j in z[2:].split("..")]
+
+	cubes.append((on, (min_x, max_x + 1), (min_y, max_y + 1), (min_z, max_z + 1)))
+
+s = 0
+for iz in range(-50, 51):
+	print(f"iz = {iz}")
+	for iy in range(-50, 51):
+		for ix in range(-50, 51):
+			state = False
+			for on, cx, cy, cz in cubes:
+				if cx[0] <= ix < cx[1] and cy[0] <= iy < cy[1] and cz[0] <= iz < cz[1]:
+					state = on
+			s += state
+
+print(s)
--- 2021/Day 22/aoc22_2.py
+++ 2021/Day 22/aoc22_2.py
@@ -2,3 +2,60 @@ file = open("aoc22_input.txt")
 l = file.read().split("\n")
 file.close()
 
+def diff_cube(c, d, on=False):
+	cx, cy, cz = c
+	dx, dy, dz = d
+	if (cx[0] >= dx[1] or dx[0] >= cx[1]) \
+		or (cy[0] >= dy[1] or dy[0] >= cy[1]) \
+		or (cz[0] >= dz[1] or dz[0] >= cz[1]):
+		return [c]
+	
+	cs = []
+	if cx[0] < dx[0]:
+		cs.append(((cx[0], dx[0]), (cy[0], cy[1]), (cz[0], cz[1])))
+	if dx[1] < cx[1]:
+		cs.append(((dx[1], cx[1]), (cy[0], cy[1]), (cz[0], cz[1])))
+	
+	min_x = max(cx[0], dx[0])
+	max_x = min(cx[1], dx[1])
+
+	if cy[0] < dy[0]:
+		cs.append(((min_x, max_x), (cy[0], dy[0]), (cz[0], cz[1])))
+	if dy[1] < cy[1]:
+		cs.append(((min_x, max_x), (dy[1], cy[1]), (cz[0], cz[1])))
+	
+	min_y = max(cy[0], dy[0])
+	max_y = min(cy[1], dy[1])
+
+	if cz[0] < dz[0]:
+		cs.append(((min_x, max_x), (min_y, max_y), (cz[0], dz[0])))
+	if dz[1] < cz[1]:
+		cs.append(((min_x, max_x), (min_y, max_y), (dz[1], cz[1])))
+	
+	return cs
+
+cubes = []
+
+for i in l:
+	on, a = i.split()
+	on = on == "on"
+	x, y, z = a.split(",")
+	min_x, max_x = [int(j) for j in x[2:].split("..")]
+	min_y, max_y = [int(j) for j in y[2:].split("..")]
+	min_z, max_z = [int(j) for j in z[2:].split("..")]
+	
+	d = ((min_x, max_x + 1), (min_y, max_y + 1), (min_z, max_z + 1))
+	new_cubes = []
+
+	for c in cubes:
+		new_cubes.extend(diff_cube(c, d, on))
+	cubes = new_cubes
+
+	if on:
+		cubes.append(d)
+
+s = 0
+for cx, cy, cz in cubes:
+	s += (cx[1] - cx[0]) * (cy[1] - cy[0]) * (cz[1] - cz[0])
+
+print(s)
