987744005c828c81169b373e16f9dcf260b1f49d
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-22 17:26:44 +0100

    Day 24 (2015)

--- 2015/Day 24/aoc24_1.py
+++ 2015/Day 24/aoc24_1.py
@@ -1,4 +1,31 @@
 file = open("aoc24_input.txt")
-l = file.read().split("\n")
+l = [int(j) for j in file.read().split("\n")]
 file.close()
 
+l = l[::-1]
+cap = sum(l) // 3
+
+def get_group(l, i, w, n):
+	if w == 0:
+		return True, []
+	if i >= len(l) or w < 0 or n == 0:
+		return False, []
+	
+	success, s = get_group(l, i + 1, w - l[i], n - 1)
+	if success:
+		return success, [l[i]] + s
+	
+	success, s = get_group(l, i + 1, w, n)
+	return success, s
+
+n = 0
+success = False
+while not success:
+	n += 1
+	success, s = get_group(l, 0, cap, n)
+
+p = 1
+for i in s:
+	p *= i
+
+print(p)
--- 2015/Day 24/aoc24_2.py
+++ 2015/Day 24/aoc24_2.py
@@ -1,4 +1,34 @@
 file = open("aoc24_input.txt")
-l = file.read().split("\n")
+l = [int(j) for j in file.read().split("\n")]
 file.close()
 
+l = l[::-1]
+cap = sum(l) // 4
+
+def get_groups(l, i, w):
+	if w == 0:
+		return [set()]
+	if i == len(l) or w < 0:
+		return []
+	
+	included = get_groups(l, i + 1, w - l[i])
+	for s in included:
+		s.add(l[i])
+	excluded = get_groups(l, i + 1, w)
+	
+	return included + excluded
+
+possible = get_groups(l, 0, cap)
+possible = sorted(possible, key=lambda x: len(x))
+n = min(len(j) for j in possible)
+
+m = 10**100
+for i in range(n):
+	p = 1
+	for j in possible[i]:
+		p *= j
+	
+	if p < m:
+		m = p
+
+print(m)
