baf440a632232cbdb35aebaf1e5b53303ec8f5c6
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-23 22:56:23 +0100

    Day 12 (2019)

--- 2019/Day 12/aoc12_1.py
+++ 2019/Day 12/aoc12_1.py
@@ -2,3 +2,32 @@ file = open("aoc12_input.txt")
 l = file.read().split("\n")
 file.close()
 
+STEPS = 1000
+
+pos = []
+for i in l:
+	x, y, z = i.split()
+	x = int(x[3:-1])
+	y = int(y[2:-1])
+	z = int(z[2:-1])
+	pos.append([x, y, z])
+
+vel = [[0, 0, 0] for _ in range(len(l))]
+
+for _ in range(STEPS):
+	for i in range(len(pos)):
+		for j in range(len(pos)):
+			if i == j:
+				continue
+
+			for k in range(3):
+				vel[i][k] += int(pos[i][k] < pos[j][k]) - int(pos[i][k] > pos[j][k])
+	
+	for i in range(len(pos)):
+		for k in range(3):
+			pos[i][k] += vel[i][k]
+
+s = 0
+for i in range(len(pos)):
+	s += sum([abs(j) for j in pos[i]]) * sum([abs(j) for j in vel[i]])
+print(s)
