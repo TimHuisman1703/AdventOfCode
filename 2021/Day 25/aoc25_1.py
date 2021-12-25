1be0d6e0f71dd9154d3c5378ba6943b949481847
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-25 06:34:51 +0100

    Day 25 (Too much text, not my style)

--- 2021/Day 25/aoc25_1.py
+++ 2021/Day 25/aoc25_1.py
@@ -2,17 +2,16 @@ file = open("aoc25_input.txt")
 l = file.read().split("\n")
 file.close()
 
-height = len(l)
-width = len(l[0])
+h, w = len(l), len(l[0])
 
-east = set()
-south = set()
-for iy in range(height):
-	for ix in range(width):
+e = set()
+s = set()
+for iy in range(h):
+	for ix in range(w):
 		if l[iy][ix] == ">":
-			east.add((ix, iy))
+			e.add((ix, iy))
 		elif l[iy][ix] == "v":
-			south.add((ix, iy))
+			s.add((ix, iy))
 
 steps = 0
 moved = True
@@ -20,22 +19,22 @@ while moved:
 	steps += 1
 	moved = False
 	
-	new_east = set(east)
-	new_south = set(south)
-	for p in east:
-		q = ((p[0] + 1) % width, p[1])
-		if not q in east and not q in south:
+	ne = set(e)
+	ns = set(s)
+	for p in e:
+		q = ((p[0] + 1) % w, p[1])
+		if not q in e and not q in s:
 			moved = True
-			new_east.add(q)
-			new_east.remove(p)
-	for p in south:
-		q = (p[0], (p[1] + 1) % height)
-		if not q in new_east and not q in south:
+			ne.add(q)
+			ne.remove(p)
+	for p in s:
+		q = (p[0], (p[1] + 1) % h)
+		if not q in ne and not q in s:
 			moved = True
-			new_south.add(q)
-			new_south.remove(p)
+			ns.add(q)
+			ns.remove(p)
 	
-	east = new_east
-	south = new_south
+	e = ne
+	s = ns
 
 print(steps)
