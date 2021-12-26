2eb286e2b58fc051f0c7b7532ba0b0b428fbe160
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-26 13:16:39 +0100

    Day 21 (2018)

--- 2018/Day 21/aoc21_2.py
+++ 2018/Day 21/aoc21_2.py
@@ -1,4 +1,25 @@
 file = open("aoc21_input.txt")
-l = file.read().split("\n")
+code = file.read().split("\n")
 file.close()
 
+fs = set()
+last = 0
+f = 0
+
+while 1:
+	e = f | 65536
+	f = 15466939
+
+	while 1:
+		f = ((f + (e & 255)) * 65899) & 16777215
+
+		if e < 256:
+			if f not in fs:
+				last = f
+				fs.add(f)
+				break
+			else:
+				print(last)
+				exit()
+		else:
+			e //= 256
