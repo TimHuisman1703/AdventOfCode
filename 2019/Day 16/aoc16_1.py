c6174eec516ee4fd50eea0717edccbad829fa4a1
Tim Huisman <t.j.huisman-1@student.tudelft.nl>
2021-12-27 22:55:59 +0100

    Day 16 (2019)

--- 2019/Day 16/aoc16_1.py
+++ 2019/Day 16/aoc16_1.py
@@ -1,4 +1,22 @@
 file = open("aoc16_input.txt")
-l = file.read().split("\n")
+s = file.read()
 file.close()
 
+import numpy as np
+
+STEPS = 100
+
+n = len(s)
+v = np.array([int(j) for j in s])
+v.resize((n, 1))
+
+M = np.zeros((n, n), int)
+for iy in range(n):
+	for ix in range(n):
+		i = (ix + 1) // (iy + 1)
+		M[iy, ix] = (i % 4 == 1) - (i % 4 == 3)
+
+for _ in range(STEPS):
+	v = abs(M @ v) % 10
+
+print("".join(str(j) for j in v.flatten()[:8]))
--- 2019/Day 16/aoc16_2.py
+++ 2019/Day 16/aoc16_2.py
@@ -1,4 +1,19 @@
 file = open("aoc16_input.txt")
-l = file.read().split("\n")
+s = file.read()
 file.close()
 
+STEPS = 100
+
+n = len(s)
+offset = int(s[:7])
+m = 10000 * n - offset
+
+l = [0] * m
+for i in range(m):
+	l[i] = int(s[(offset + i) % n])
+
+for steps in range(STEPS):
+	for i in range(m - 2, -1, -1):
+		l[i] = (l[i] + l[i+1]) % 10
+
+print(*l[:8], sep="")
