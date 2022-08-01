947911f1b3895b01bd7a02e57b1efbb42890aedc
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-08-01 13:04:50 +0200

    Day 22 (2019): Make calculation direct

--- 2019/Day 22/aoc22_2.py
+++ 2019/Day 22/aoc22_2.py
@@ -2,18 +2,6 @@ file = open("aoc22_input.txt")
 l = file.read().split("\n")
 file.close()
 
-def pow(a, b, mod):
-	if a < 2:
-		return a
-	if b == 0:
-		return 1
-	
-	if b % 2 == 0:
-		c = pow(a, b // 2, mod)
-		return c * c % mod
-	else:
-		return a * pow(a, b - 1, mod) % mod
-
 def geom_series(r, n, mod):
 	if n == 1:
 		return 1
@@ -49,6 +37,4 @@ for i in range(2):
 	goes_to.append((c * geom_sum + r_to_nth * i) % CARDS)
 r, c = (goes_to[1] - goes_to[0]) % CARDS, goes_to[0]
 
-
-print("Put this in a congruence solver:")
-print(f"({r} * k + {c}) % {CARDS} == 2020")
+print((2020 - c)  * pow(r, CARDS - 2, CARDS) % CARDS)
