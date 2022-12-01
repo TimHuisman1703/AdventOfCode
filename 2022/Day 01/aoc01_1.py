fb5b7cd3c83c4066a0696a29562fea2e6b3cea17
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-01 09:26:25 +0100

    Day 1

--- 2022/Day 01/aoc01_1.py
+++ 2022/Day 01/aoc01_1.py
@@ -2,3 +2,15 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+m = 0
+
+for i in l:
+    if i == "":
+        m = max(m, s)
+        s = 0
+    else:
+        s += int(i)
+m = max(m, s)
+
+print(m)
--- 2022/Day 01/aoc01_2.py
+++ 2022/Day 01/aoc01_2.py
@@ -2,3 +2,17 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+a = []
+
+for i in l:
+    if i == "":
+        a.append(s)
+        s = 0
+    else:
+        s += int(i)
+a.append(s)
+
+a = sorted(a)
+
+print(sum(a[-3:]))
