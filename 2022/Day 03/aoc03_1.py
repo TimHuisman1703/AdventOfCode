b82250a4f9221bc215ae05a0611512188e19bc3f
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-03 10:09:29 +0100

    Day 3

--- 2022/Day 03/aoc03_1.py
+++ 2022/Day 03/aoc03_1.py
@@ -2,3 +2,13 @@ file = open("aoc03_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for i in l:
+    a, b = i[:len(i) // 2], i[len(i) // 2:]
+    c = [*set([*a]) & set([*b])][0]
+    if c.isupper():
+        s += ord(c) - ord("A") + 27
+    else:
+        s += ord(c) - ord("a") + 1
+
+print(s)
--- 2022/Day 03/aoc03_2.py
+++ 2022/Day 03/aoc03_2.py
@@ -2,3 +2,12 @@ file = open("aoc03_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for i in range(0, len(l), 3):
+    c = [*set([*l[i]]) & set([*l[i + 1]]) & set([*l[i + 2]])][0]
+    if c.isupper():
+        s += ord(c) - ord("A") + 27
+    else:
+        s += ord(c) - ord("a") + 1
+
+print(s)
