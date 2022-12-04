82d4e81929303488dbd3c6ce8177f32b825756ea
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-04 10:04:43 +0100

    Day 4

--- 2022/Day 04/aoc04_1.py
+++ 2022/Day 04/aoc04_1.py
@@ -2,3 +2,14 @@ file = open("aoc04_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+
+for i in l:
+    p, q = i.split(",")
+    a = [int(j) for j in p.split("-")]
+    b = [int(j) for j in q.split("-")]
+
+    a, b = sorted([a, b], key=lambda x: (x[0], -x[1]))
+    s += b[1] <= a[1]
+
+print(s)
