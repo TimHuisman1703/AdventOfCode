f4b38f4ff07ab38aef25ce38f4e560a6acb93e56
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-08 21:54:38 +0100

    Day 8

--- 2022/Day 08/aoc08_1.py
+++ 2022/Day 08/aoc08_1.py
@@ -1,4 +1,15 @@
 file = open("aoc08_input.txt")
-l = file.read().split("\n")
+g = [[[int(j), 0] for j in row] for row in file.read().split("\n")]
 file.close()
 
+for i in range(4):
+    for iy in range(len(g)):
+        m = -1
+        for ix in range(len(g[0])):
+            if g[iy][ix][0] > m:
+                g[iy][ix][1] = 1
+                m = g[iy][ix][0]
+    
+    g = [[g[ix][len(g) - 1 - iy] for ix in range(len(g[0]))] for iy in range(len(g))]
+
+print(sum(sum([j[1] for j in row]) for row in g))
--- 2022/Day 08/aoc08_2.py
+++ 2022/Day 08/aoc08_2.py
@@ -1,4 +1,29 @@
 file = open("aoc08_input.txt")
-l = file.read().split("\n")
+g = [[int(j) for j in row] for row in file.read().split("\n")]
 file.close()
 
+m = 0
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        p = 1
+
+        for i in range(4):
+            x, y = ix, iy
+            c = 0
+
+            while 1:
+                x += (i == 0) - (i == 2)
+                y += (i == 1) - (i == 3)
+
+                if x < 0 or x >= len(g[0]) or y < 0 or y >= len(g):
+                    break
+
+                c += 1
+                if g[y][x] >= g[iy][ix]:
+                    break
+
+            p *= c
+
+        m = max(p, m)
+
+print(m)
