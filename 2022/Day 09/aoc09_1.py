93127a8c4d82105eedaa22ccba49335f81891b01
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-09 11:50:15 +0100

    Day 9

--- 2022/Day 09/aoc09_1.py
+++ 2022/Day 09/aoc09_1.py
@@ -2,3 +2,22 @@ file = open("aoc09_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = set([(0, 0)])
+
+hx = hy = 0
+tx = ty = 0
+
+for i in l:
+    a, b = i.split()
+    b = int(b)
+
+    for _ in range(b):
+        hx += (a == "R") - (a == "L")
+        hy += (a == "D") - (a == "U")
+        
+        if abs(tx - hx) > 1 or abs(ty -  hy) > 1:
+            tx += (tx < hx) - (tx > hx)
+            ty += (ty < hy) - (ty > hy)
+        s.add((tx, ty))
+
+print(len(s))
--- 2022/Day 09/aoc09_2.py
+++ 2022/Day 09/aoc09_2.py
@@ -2,3 +2,23 @@ file = open("aoc09_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = set([(0, 0)])
+
+x = [0] * 10
+y = [0] * 10
+
+for i in l:
+    a, b = i.split()
+    b = int(b)
+
+    for _ in range(b):
+        x[0] += (a == "R") - (a == "L")
+        y[0] += (a == "D") - (a == "U")
+        
+        for j in range(len(x) - 1):
+            if abs(x[j + 1] - x[j]) > 1 or abs(y[j + 1] -  y[j]) > 1:
+                x[j + 1] += (x[j + 1] < x[j]) - (x[j + 1] > x[j])
+                y[j + 1] += (y[j + 1] < y[j]) - (y[j + 1] > y[j])
+        s.add((x[-1], y[-1]))
+
+print(len(s))
