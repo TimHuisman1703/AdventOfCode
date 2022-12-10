c0aa456ec1e11bf2e3297f89b25d68ee598392e6
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-10 12:29:44 +0100

    Day 10

--- 2022/Day 10/aoc10_1.py
+++ 2022/Day 10/aoc10_1.py
@@ -2,3 +2,24 @@ file = open("aoc10_input.txt")
 l = file.read().split("\n")
 file.close()
 
+i = 0
+x = 1
+c = 0
+a = 10 ** 10
+
+s = 0
+
+while i < len(l):
+    if c % 40 == 19:
+        s += x * (c + 1)
+    c += 1
+    
+    if a != 10 ** 10:
+        x += a
+        a = 10 ** 10
+    else:
+        if l[i] != "noop":
+            a = int(l[i].split()[1])
+        i += 1
+
+print(s)
--- 2022/Day 10/aoc10_2.py
+++ 2022/Day 10/aoc10_2.py
@@ -2,3 +2,24 @@ file = open("aoc10_input.txt")
 l = file.read().split("\n")
 file.close()
 
+i = 0
+x = 1
+c = 0
+a = 10 ** 10
+
+g = [["." for _ in range(40)] for _ in range(6)]
+
+while i < len(l):
+    if abs(c % 40 - x) <= 1:
+        g[c // 40][c % 40] = "#"
+    c += 1
+    
+    if a != 10 ** 10:
+        x += a
+        a = 10 ** 10
+    else:
+        if l[i] != "noop":
+            a = int(l[i].split()[1])
+        i += 1
+
+print("\n".join("".join(row) for row in g))
