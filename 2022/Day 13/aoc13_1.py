8c4050c9c5feba942a4ee65745d462d9462e2bd3
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-13 09:58:21 +0100

    Day 13

--- 2022/Day 13/aoc13_1.py
+++ 2022/Day 13/aoc13_1.py
@@ -2,3 +2,27 @@ file = open("aoc13_input.txt")
 l = file.read().split("\n")
 file.close()
 
+def f(a, b):
+    if type(a) == type(b) == int:
+        return (a > b) - (a < b)
+    
+    if type(a) == int:
+        a = [a]
+    elif type(b) == int:
+        b = [b]
+    
+    for i in range(min(len(a), len(b))):
+        x = f(a[i], b[i])
+        if x != 0:
+            return x
+    return (len(a) > len(b)) - (len(a) < len(b))
+
+c = 0
+for i in range(0, len(l), 3):
+    a = eval(l[i])
+    b = eval(l[i + 1])
+
+    if f(a, b) <= 0:
+        c += i // 3 + 1
+
+print(c)
--- 2022/Day 13/aoc13_2.py
+++ 2022/Day 13/aoc13_2.py
@@ -2,3 +2,32 @@ file = open("aoc13_input.txt")
 l = file.read().split("\n")
 file.close()
 
+def f(a, b):
+    if type(a) == type(b) == int:
+        return (a > b) - (a < b)
+    
+    if type(a) == int:
+        a = [a]
+    elif type(b) == int:
+        b = [b]
+    
+    for i in range(min(len(a), len(b))):
+        x = f(a[i], b[i])
+        if x != 0:
+            return x
+    return (len(a) > len(b)) - (len(a) < len(b))
+
+p = [[[2]], [[6]]]
+for i in range(0, len(l), 3):
+    p.append(eval(l[i]))
+    p.append(eval(l[i + 1]))
+
+i = 0
+while i < len(p) - 1:
+    if f(p[i], p[i + 1]) == 1:
+        p[i], p[i + 1] = p[i + 1], p[i]
+        i = max(0, i - 1)
+    else:
+        i += 1
+
+print((p.index([[2]]) + 1) * (p.index([[6]]) + 1))
