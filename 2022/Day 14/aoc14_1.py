0583ae4f69083d9fae0ba4bc43b1bcab410580d1
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-14 15:17:34 +0100

    Day 14

--- 2022/Day 14/aoc14_1.py
+++ 2022/Day 14/aoc14_1.py
@@ -2,3 +2,37 @@ file = open("aoc14_input.txt")
 l = file.read().split("\n")
 file.close()
 
+g = [[0 for _ in range(1000)] for _ in range(200)]
+
+for i in l:
+    c = [[int(k) for k in j.split(",")] for j in i.split(" -> ")]
+    for i in range(len(c) - 1):
+        x, y = c[i]
+        dx, dy = c[i + 1]
+        g[y][x] = 1
+        while x != dx or y != dy:
+            x += (x < dx) - (x > dx)
+            y += (y < dy) - (y > dy)
+            g[y][x] = 1
+
+c = 0
+while 1:
+    x, y = 500, 0
+
+    while y < len(g) - 1:
+        if g[y + 1][x] == 0:
+            pass
+        elif g[y + 1][x - 1] == 0:
+            x -= 1
+        elif g[y + 1][x + 1] == 0:
+            x += 1
+        else:
+            break
+        y += 1
+    else:
+        break
+
+    c += 1
+    g[y][x] = 1
+
+print(c)
--- 2022/Day 14/aoc14_2.py
+++ 2022/Day 14/aoc14_2.py
@@ -2,3 +2,40 @@ file = open("aoc14_input.txt")
 l = file.read().split("\n")
 file.close()
 
+g = [[0 for _ in range(1000)] for _ in range(200)]
+my = 0
+
+for i in l:
+    c = [[int(k) for k in j.split(",")] for j in i.split(" -> ")]
+    for i in range(len(c) - 1):
+        x, y = c[i]
+        dx, dy = c[i + 1]
+        g[y][x] = 1
+        while x != dx or y != dy:
+            x += (x < dx) - (x > dx)
+            y += (y < dy) - (y > dy)
+            g[y][x] = 1
+        my = max([my, y, dy])
+
+for ix in range(len(g[0])):
+    g[my + 2][ix] = 1
+
+c = 0
+while g[0][500] == 0:
+    x, y = 500, 0
+
+    while 1:
+        if g[y + 1][x] == 0:
+            pass
+        elif g[y + 1][x - 1] == 0:
+            x -= 1
+        elif g[y + 1][x + 1] == 0:
+            x += 1
+        else:
+            break
+        y += 1
+
+    c += 1
+    g[y][x] = 1
+
+print(c)
