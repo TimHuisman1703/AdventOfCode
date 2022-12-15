1dff102b0190b42feda2bd6195a671a139209724
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-15 22:46:54 +0100

    Day 15

--- 2022/Day 15/aoc15_1.py
+++ 2022/Day 15/aoc15_1.py
@@ -2,3 +2,21 @@ file = open("aoc15_input.txt")
 l = file.read().split("\n")
 file.close()
 
+Y = 2000000
+
+c = [0 for _ in range(20000000)]
+
+for i in l:
+    i = i.split()
+    sx, sy = int(i[2][2:-1]), int(i[3][2:-1])
+    bx, by = int(i[8][2:-1]), int(i[9][2:])
+
+    if by == Y:
+        c[bx] = 2
+
+    r = abs(sx - bx) + abs(by - sy)
+    w = r - abs(sy - Y)
+    for i in range(-w, w + 1):
+        c[sx - i] = max(c[sx - i], 1)
+
+print(c.count(1))
--- 2022/Day 15/aoc15_2.py
+++ 2022/Day 15/aoc15_2.py
@@ -2,3 +2,32 @@ file = open("aoc15_input.txt")
 l = file.read().split("\n")
 file.close()
 
+M = 4000000
+
+s = []
+
+for i in l:
+    i = i.split()
+    sx, sy = int(i[2][2:-1]), int(i[3][2:-1])
+    bx, by = int(i[8][2:-1]), int(i[9][2:])
+
+    r = abs(sx - bx) + abs(by - sy)
+    s.append((sx, sy, r))
+
+for i in range(len(s)):
+    ax, ay, ar = s[i]
+    print(f"{i + 1}/{len(s)}")
+    
+    for nx in range(max(0, ax - ar - 1), min(M + 1, ax + ar + 2)):
+        for side in range(1 + (-ar <= nx <= ar)):
+            ny = ay + (ar + 1 - abs(nx - ax)) * (2 * side - 1)
+            if ny < 0 or ny > M:
+                continue
+
+            for b in s:
+                bx, by, br = b
+                if abs(bx - nx) + abs(by - ny) <= br:
+                    break
+            else:
+                print(nx * 4000000 + ny)
+                exit()
