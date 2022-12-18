6719198f12a1348acaa7e5fbe096bd509b04dcd7
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-18 10:10:28 +0100

    Day 18

--- 2022/Day 18/aoc18_1.py
+++ 2022/Day 18/aoc18_1.py
@@ -2,3 +2,17 @@ file = open("aoc18_input.txt")
 l = file.read().split("\n")
 file.close()
 
+c = [tuple(int(k) for k in j.split(",")) for j in l]
+
+v = set()
+s = 6 * len(c)
+
+for curr in c:
+    v.add(curr)
+
+    for j in range(6):
+        x, y, z = [curr[k] - (k == j) + (k + 3 == j) for k in range(3)]
+        if (x, y, z) in v:
+            s -= 2
+
+print(s)
--- 2022/Day 18/aoc18_2.py
+++ 2022/Day 18/aoc18_2.py
@@ -2,3 +2,26 @@ file = open("aoc18_input.txt")
 l = file.read().split("\n")
 file.close()
 
+c = set(tuple(int(k) for k in j.split(",")) for j in l)
+
+q = [(0, 0, 0)]
+v = set()
+s = 0
+
+while q:
+    curr = q.pop()
+
+    if curr in c:
+        s += 1
+        continue
+    
+    if curr in v:
+        continue
+    v.add(curr)
+
+    for j in range(6):
+        x, y, z = [curr[k] - (k == j) + (k + 3 == j) for k in range(3)]
+        if -5 <= x <= 25 and -5 <= y <= 25 and -5 <= z <= 25:
+            q.append((x, y, z))
+
+print(s)
