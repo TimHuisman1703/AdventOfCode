95ed7d2cb1d0e2b268752f253020a2df58e67026
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2022-12-25 06:24:32 +0100

    Day 25 (Merry Christmas!)

--- 2022/Day 25/aoc25_1.py
+++ 2022/Day 25/aoc25_1.py
@@ -2,3 +2,18 @@ file = open("aoc25_input.txt")
 l = file.read().split("\n")
 file.close()
 
+def f(s):
+    r = 0
+    for i in s:
+        r = 5 * r + "=-012".index(i) - 2
+    return r
+
+def g(n):
+    s = ""
+    while n != 0:
+        x = (n + 2) % 5 - 2
+        s = "012=-"[x] + s
+        n = (n - x) // 5
+    return s
+
+print(g(sum(f(j) for j in l)))
