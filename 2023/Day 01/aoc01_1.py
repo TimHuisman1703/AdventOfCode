a39951b78f0b45f67a023ddfd46f91b0a50b57ce
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-01 10:34:35 +0100

    Day 01

--- 2023/Day 01/aoc01_1.py
+++ 2023/Day 01/aoc01_1.py
@@ -2,3 +2,10 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+l = ["".join(j for j in w if j.isdigit()) for w in l]
+
+s = 0
+for w in l:
+    s += int(w[0]) * 10 + int(w[-1])
+
+print(s)
--- 2023/Day 01/aoc01_2.py
+++ 2023/Day 01/aoc01_2.py
@@ -2,3 +2,37 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+d = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "0": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+s = 0
+for w in l:
+    first = last = -1
+    for i in range(len(w)):
+        for key, value in d.items():
+            if w[i:i + len(key)] == key:
+                if first == -1:
+                    first = value
+                last = value
+    s += first * 10 + last
+
+print(s)
