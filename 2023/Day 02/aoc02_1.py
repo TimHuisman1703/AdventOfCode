fb124de00da5b2a6638c27533122fabaa6367704
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-02 10:46:17 +0100

    Day 02

--- 2023/Day 02/aoc02_1.py
+++ 2023/Day 02/aoc02_1.py
@@ -2,3 +2,22 @@ file = open("aoc02_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for i in range(len(l)):
+    b = l[i].split(": ")[1].split("; ")
+    pos = True
+
+    for g in b:
+        count = {}
+        for w in g.split(", "):
+            n, t = w.split()
+            count[t] = count.get(t, 0) + int(n)
+
+        if count.get("red", 0) > 12 or count.get("green", 0) > 13 or count.get("blue", 0) > 14:
+            pos = False
+            break
+
+    if pos:
+        s += i + 1
+
+print(s)
--- 2023/Day 02/aoc02_2.py
+++ 2023/Day 02/aoc02_2.py
@@ -2,3 +2,21 @@ file = open("aoc02_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for i in range(len(l)):
+    b = l[i].split(": ")[1].split("; ")
+    min_count = {"red": 0, "green": 0, "blue": 0}
+
+    for g in b:
+        count = {}
+        for w in g.split(", "):
+            n, t = w.split()
+            count[t] = count.get(t, 0) + int(n)
+
+        min_count["red"] = max(min_count["red"], count.get("red", 0))
+        min_count["green"] = max(min_count["green"], count.get("green", 0))
+        min_count["blue"] = max(min_count["blue"], count.get("blue", 0))
+
+    s += min_count["red"] * min_count["green"] * min_count["blue"]
+
+print(s)
