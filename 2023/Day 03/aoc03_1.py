0418a80966733d0783efa78aab7b02dfd394b40c
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-03 10:42:40 +0100

    Day 03

--- 2023/Day 03/aoc03_1.py
+++ 2023/Day 03/aoc03_1.py
@@ -1,4 +1,40 @@
 file = open("aoc03_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+w, h = len(g[0]), len(g)
+
+q = []
+for iy in range(h):
+    for ix in range(w):
+        if not g[iy][ix].isdigit() and g[iy][ix] != ".":
+            q.append((ix, iy))
+
+visited = [[False for _ in range(w)] for _ in range(h)]
+while q:
+    x, y = q.pop()
+
+    if g[y][x] == ".":
+        continue
+
+    if visited[y][x]:
+        continue
+    visited[y][x] = True
+
+    for iy in range(y - 1, y + 2):
+        for ix in range(x - 1, x + 2):
+            if ix < 0 or ix >= w or iy < 0 or iy >= h:
+                continue
+            q.append((ix, iy))
+
+s = 0
+c = 0
+for iy in range(h):
+    for ix in range(w + 1):
+        if ix < w and g[iy][ix].isdigit() and visited[iy][ix]:
+            c = 10 * c + int(g[iy][ix])
+        else:
+            s += c
+            c = 0
+
+print(s)
--- 2023/Day 03/aoc03_2.py
+++ 2023/Day 03/aoc03_2.py
@@ -1,4 +1,50 @@
 file = open("aoc03_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+w, h = len(g[0]), len(g)
+
+symbols = []
+for iy in range(h):
+    for ix in range(w):
+        if not g[iy][ix].isdigit() and g[iy][ix] != ".":
+            symbols.append((ix, iy))
+
+s = 0
+visited = [[False for _ in range(w)] for _ in range(h)]
+for sym in symbols:
+    q = [sym]
+    starts = set()
+
+    while q:
+        x, y = q.pop()
+
+        if g[y][x] == ".":
+            continue
+
+        if visited[y][x]:
+            continue
+        visited[y][x] = True
+
+        if g[y][x].isdigit():
+            starts.add((x, y))
+
+        for iy in range(y - 1, y + 2):
+            for ix in range(x - 1, x + 2):
+                if ix < 0 or ix >= w or iy < 0 or iy >= h:
+                    continue
+                q.append((ix, iy))
+
+    starts = {(x, y) for (x, y) in starts if (x - 1, y) not in starts}
+
+    if len(starts) == 2:
+        p = 1
+        for x, y in starts:
+            c = 0
+            while x < w and g[y][x].isdigit():
+                c = 10 * c + int(g[y][x])
+                x += 1
+            p *= c
+        s += p
+
+print(s)
