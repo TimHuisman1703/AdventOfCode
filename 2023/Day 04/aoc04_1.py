e160e46c14764c3908e0ae08c2228290aa7d687b
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-04 09:10:11 +0100

    Day 04

--- 2023/Day 04/aoc04_1.py
+++ 2023/Day 04/aoc04_1.py
@@ -2,3 +2,13 @@ file = open("aoc04_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for w in l:
+    w = w.split(": ")[1]
+    a, b = [{int(j) for j in row.split()} for row in w.split(" | ")]
+
+    p = len(a & b)
+    if p:
+        s += 1 << (p - 1)
+
+print(s)
--- 2023/Day 04/aoc04_2.py
+++ 2023/Day 04/aoc04_2.py
@@ -2,3 +2,13 @@ file = open("aoc04_input.txt")
 l = file.read().split("\n")
 file.close()
 
+d = [1] * len(l)
+for idx, w in enumerate(l):
+    w = w.split(": ")[1]
+    a, b = [{int(j) for j in row.split()} for row in w.split(" | ")]
+
+    p = len(a & b)
+    for i in range(idx + 1, idx + 1 + p):
+        d[i] += d[idx]
+
+print(sum(d))
