b22d8b46c57c7d14d50b3b9c8b78fb00f981d91a
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-06 09:57:25 +0100

    Day 06

--- 2023/Day 06/aoc06_1.py
+++ 2023/Day 06/aoc06_1.py
@@ -2,3 +2,16 @@ file = open("aoc06_input.txt")
 l = file.read().split("\n")
 file.close()
 
+ts = [int(j) for j in l[0].split()[1:]]
+ds = [int(j) for j in l[1].split()[1:]]
+
+p = 1
+for t, d in zip(ts, ds):
+    s = 0
+    for i in range(0, t):
+        b = i * (t - i)
+        if b > d:
+            s += 1
+    p *= s
+
+print(p)
--- 2023/Day 06/aoc06_2.py
+++ 2023/Day 06/aoc06_2.py
@@ -2,3 +2,17 @@ file = open("aoc06_input.txt")
 l = file.read().split("\n")
 file.close()
 
+t = int("".join(l[0].split()[1:]).replace(" ", ""))
+d = int("".join(l[1].split()[1:]).replace(" ", ""))
+
+a = 0
+b = t // 2
+while a != b:
+    mid = (a + b) // 2
+
+    if mid * (t - mid) > d:
+        b = mid
+    else:
+        a = mid + 1
+
+print(t - a * 2 + 1)
