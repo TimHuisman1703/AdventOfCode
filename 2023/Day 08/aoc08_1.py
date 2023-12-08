50d440d67ca9e516d3b49c3e516f4a7a8355109b
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-08 10:50:06 +0100

    Day 08

--- 2023/Day 08/aoc08_1.py
+++ 2023/Day 08/aoc08_1.py
@@ -2,3 +2,17 @@ file = open("aoc08_input.txt")
 l = file.read().split("\n")
 file.close()
 
+instrs = l[0]
+children = {}
+for w in l[2:]:
+    p, c = w.split(" = ")
+    children[p] = c[1:-1].split(", ")
+
+node = "AAA"
+
+idx = 0
+while node != "ZZZ":
+    node = children[node][instrs[idx % len(instrs)] == "R"]
+    idx += 1
+
+print(idx)
--- 2023/Day 08/aoc08_2.py
+++ 2023/Day 08/aoc08_2.py
@@ -2,3 +2,23 @@ file = open("aoc08_input.txt")
 l = file.read().split("\n")
 file.close()
 
+from math import lcm
+
+instrs = l[0]
+children = {}
+for w in l[2:]:
+    p, c = w.split(" = ")
+    children[p] = c[1:-1].split(", ")
+
+p = 1
+nodes = [j for j in children.keys() if j[-1] == "A"]
+for node in nodes:
+    idx = 0
+    while True:
+        node = children[node][instrs[idx % len(instrs)] == "R"]
+        idx += 1
+        if node[-1] == "Z":
+            break
+    p = lcm(idx, p)
+
+print(p)
