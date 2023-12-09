5c8a1b2f0af3be8c57cd8c9ab1d9ca05e9344e4a
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-09 11:54:31 +0100

    Day 09

--- 2023/Day 09/aoc09_1.py
+++ 2023/Day 09/aoc09_1.py
@@ -2,3 +2,12 @@ file = open("aoc09_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for w in l:
+    d = [int(j) for j in w.split()]
+
+    while any(d):
+        s += d[-1]
+        d = [d[j + 1] - d[j] for j in range(len(d) - 1)]
+
+print(s)
--- 2023/Day 09/aoc09_2.py
+++ 2023/Day 09/aoc09_2.py
@@ -2,3 +2,14 @@ file = open("aoc09_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 0
+for w in l:
+    d = [int(j) for j in w.split()]
+
+    sign = 1
+    while any(d):
+        s += d[0] * sign
+        d = [d[j + 1] - d[j] for j in range(len(d) - 1)]
+        sign *= -1
+
+print(s)
