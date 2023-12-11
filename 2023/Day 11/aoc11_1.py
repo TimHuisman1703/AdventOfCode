543009c24e9b6daf2d09d0f0a7133fe98a2f084f
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-11 11:40:36 +0100

    Day 11

--- 2023/Day 11/aoc11_1.py
+++ 2023/Day 11/aoc11_1.py
@@ -2,3 +2,31 @@ file = open("aoc11_input.txt")
 l = file.read().split("\n")
 file.close()
 
+g = [[l[iy][ix] == "#" for ix in range(len(l[0]))] for iy in range(len(l))]
+
+ng = [[] for _ in range(len(g))]
+for ix in range(len(g[0])):
+    for iy in range(len(g)):
+        ng[iy].append(g[iy][ix])
+    if not any(g[iy][ix] for iy in range(len(g))):
+        for iy in range(len(g)):
+            ng[iy].append(False)
+g = ng
+
+ng = []
+for iy in range(len(g)):
+    ng.append(g[iy])
+    if not any(g[iy]):
+        ng.append(g[iy][:])
+g = ng
+
+ps = []
+s = 0
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if g[iy][ix]:
+            for jx, jy in ps:
+                s += abs(jx - ix) + abs(jy - iy)
+            ps.append((ix, iy))
+
+print(s)
--- 2023/Day 11/aoc11_2.py
+++ 2023/Day 11/aoc11_2.py
@@ -2,3 +2,27 @@ file = open("aoc11_input.txt")
 l = file.read().split("\n")
 file.close()
 
+g = [[l[iy][ix] == "#" for ix in range(len(l[0]))] for iy in range(len(l))]
+
+xs = []
+for ix in range(len(g[0])):
+    if not any(g[iy][ix] for iy in range(len(g))):
+        xs.append(ix)
+
+ys = []
+for iy in range(len(g)):
+    if not any(g[iy]):
+        ys.append(iy)
+
+ps = []
+s = 0
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if g[iy][ix]:
+            for jx, jy in ps:
+                s += abs(jx - ix) + abs(jy - iy)
+                s += 999999 * sum(iy < y < jy or jy < y < iy for y in ys)
+                s += 999999 * sum(ix < x < jx or jx < x < ix for x in xs)
+            ps.append((ix, iy))
+
+print(s)
