feb0475b77639a660630673de23104281bb086dc
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-13 09:12:55 +0100

    Day 13

--- 2023/Day 13/aoc13_1.py
+++ 2023/Day 13/aoc13_1.py
@@ -1,4 +1,23 @@
 file = open("aoc13_input.txt")
-l = file.read().split("\n")
+l = file.read().split("\n\n")
 file.close()
 
+s = 0
+for g in l:
+    g = g.split("\n")
+
+    for mult in [1, 100]:
+        g = [[g[iy][ix] for iy in range(len(g))] for ix in range(len(g[0]))]
+
+        for iy in range(1, len(g)):
+            a = iy - 1
+            b = iy
+            while a >= 0 and b < len(g):
+                if g[a] != g[b]:
+                    break
+                a -= 1
+                b += 1
+            else:
+                s += iy * mult
+
+print(s)
--- 2023/Day 13/aoc13_2.py
+++ 2023/Day 13/aoc13_2.py
@@ -1,4 +1,25 @@
 file = open("aoc13_input.txt")
-l = file.read().split("\n")
+l = file.read().split("\n\n")
 file.close()
 
+s = 0
+for g in l:
+    g = g.split("\n")
+
+    for mult in [1, 100]:
+        g = [[g[iy][ix] for iy in range(len(g))] for ix in range(len(g[0]))]
+
+        for iy in range(1, len(g)):
+            a = iy - 1
+            b = iy
+            diff = 0
+            while a >= 0 and b < len(g):
+                diff += sum(g[a][j] != g[b][j] for j in range(len(g[a])))
+                if diff > 1:
+                    break
+                a -= 1
+                b += 1
+            else:
+                s += iy * mult * diff
+
+print(s)
