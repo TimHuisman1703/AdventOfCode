f01f3bbede976f4c3eab20072694a7237c9d07c1
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-17 10:43:39 +0100

    Day 16

--- 2023/Day 16/aoc16_1.py
+++ 2023/Day 16/aoc16_1.py
@@ -1,4 +1,42 @@
 file = open("aoc16_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+ps = set()
+
+x = y = 0
+dx = 1
+dy = 0
+
+q = [(0, 0, 0, 1)]
+while q:
+    nq = []
+    for x, y, dx, dy in q:
+        if (x, y, dx, dy) in ps:
+            continue
+        ps.add((x, y, dx, dy))
+
+        x += dx
+        y += dy
+
+        if x < 0 or x >= len(g[0]) or y < 0 or y >= len(g):
+            continue
+
+        if g[y][x] == "/":
+            nq.append((x, y, -dy, -dx))
+        elif g[y][x] == "\\":
+            nq.append((x, y, dy, dx))
+        elif g[y][x] == "-" and dx == 0:
+            nq.append((x, y, 1, 0))
+            nq.append((x, y, -1, 0))
+        elif g[y][x] == "|" and dy == 0:
+            nq.append((x, y, 0, 1))
+            nq.append((x, y, 0, -1))
+        elif g[y][x] == "-" or g[y][x] == "|":
+            nq.append((x, y, dx, dy))
+        elif g[y][x] == ".":
+            nq.append((x, y, dx, dy))
+
+    q = nq
+
+print(len(set([(x, y) for x, y, _, _ in ps])))
--- 2023/Day 16/aoc16_2.py
+++ 2023/Day 16/aoc16_2.py
@@ -1,4 +1,59 @@
 file = open("aoc16_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+b = 0
+
+for sy in range(len(g)):
+    for sx in range(len(g[0])):
+        for d in range(4):
+            dx = (d == 0) - (d == 1)
+            dy = (d == 2) - (d == 3)
+
+            pos = False
+            if sx == 0 and dx > 0:
+                pos = True
+            if sx == len(g[0]) - 1 and dx < 0:
+                pos = True
+            if sy == 0 and dy > 0:
+                pos = True
+            if sy == len(g) - 1 and dy < 0:
+                pos = True
+            if not pos:
+                continue
+
+            q = [(sx, sy, dx, dy)]
+            ps = set()
+
+            while q:
+                nq = []
+                for x, y, dx, dy in q:
+                    if x < 0 or x >= len(g[0]) or y < 0 or y >= len(g):
+                        continue
+
+                    if (x, y, dx, dy) in ps:
+                        continue
+                    ps.add((x, y, dx, dy))
+
+                    ds = []
+                    if g[y][x] == "/":
+                        ds.append((-dy, -dx))
+                    elif g[y][x] == "\\":
+                        ds.append((dy, dx))
+                    elif g[y][x] == "-" and dx == 0:
+                        ds.append((1, 0))
+                        ds.append((-1, 0))
+                    elif g[y][x] == "|" and dy == 0:
+                        ds.append((0, 1))
+                        ds.append((0, -1))
+                    else:
+                        ds.append((dx, dy))
+
+                    nq.extend([(x + dx, y + dy, dx, dy) for dx, dy in ds])
+
+                q = nq
+
+            c = len(set([(x, y) for x, y, _, _ in ps]))
+            b = max(b, c)
+
+print(b)
