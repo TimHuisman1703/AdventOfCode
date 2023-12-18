ffe3c788bf6753ec5421a08d6511c061c5e23718
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-18 09:05:21 +0100

    Day 18

--- 2023/Day 18/aoc18_1.py
+++ 2023/Day 18/aoc18_1.py
@@ -2,3 +2,33 @@ file = open("aoc18_input.txt")
 l = file.read().split("\n")
 file.close()
 
+wall = set([(0, 0)])
+x = y = 0
+for w in l:
+    d, s, c = w.split()
+    s = int(s)
+
+    for _ in range(s):
+        x += (d == "R") - (d == "L")
+        y += (d == "D") - (d == "U")
+        wall.add((x, y))
+
+q = [(1, 1)]
+inside = set()
+while q:
+    x, y = q.pop()
+
+    if (x, y) in wall:
+        continue
+
+    if (x, y) in inside:
+        continue
+    inside.add((x, y))
+
+    for d in range(4):
+        q.append((
+            x + (d == 0) - (d == 1),
+            y + (d == 2) - (d == 3),
+        ))
+
+print(len(inside) + len(wall))
--- 2023/Day 18/aoc18_2.py
+++ 2023/Day 18/aoc18_2.py
@@ -2,3 +2,24 @@ file = open("aoc18_input.txt")
 l = file.read().split("\n")
 file.close()
 
+corners = []
+edge = 0
+x = y = 0
+for w in l:
+    d, s, c = w.split()
+    d = "RDLU"[int(c[7])]
+    s = int(c[2:7], 16)
+
+    edge += s
+
+    x += ((d == "R") - (d == "L")) * s
+    y += ((d == "D") - (d == "U")) * s
+
+    corners.append((x, y))
+
+area = 0
+for i in range(len(corners) - 2):
+    a, b = corners[i], corners[i + 1]
+    area += (a[0] * b[1] - b[0] * a[1]) // 2
+
+print(abs(area) + edge // 2 + 1)
