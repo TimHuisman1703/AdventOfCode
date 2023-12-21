95ef44b8d36a1f377f1e18982955899c782756bf
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-21 12:48:39 +0100

    Day 21

--- 2023/Day 21/aoc21_1.py
+++ 2023/Day 21/aoc21_1.py
@@ -1,4 +1,27 @@
 file = open("aoc21_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+p = set()
+walls = set()
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if g[iy][ix] == "S":
+            p.add((ix, iy))
+        if g[iy][ix] == "#":
+            walls.add((ix, iy))
+
+for _ in range(64):
+    np = set()
+    for x, y in p:
+        for d in range(4):
+            nx = x + (d == 0) - (d == 1)
+            ny = y + (d == 2) - (d == 3)
+
+            key = (nx, ny)
+            if key not in walls:
+                np.add(key)
+
+    p = np
+
+print(len(p))
--- 2023/Day 21/aoc21_2.py
+++ 2023/Day 21/aoc21_2.py
@@ -1,4 +1,119 @@
 file = open("aoc21_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+g = [list(j) for j in g]
+w, h = len(g[0]), len(g)
+
+sx = sy = 0
+for iy in range(h):
+    for ix in range(w):
+        if g[iy][ix] == "S":
+            sx, sy = ix, iy
+        g[iy][ix] = g[iy][ix] == "#"
+
+g = [[g[jy % h][jx % w] for jx in range(2 * w)] for jy in range(2 * h)]
+w *= 2
+h *= 2
+
+timeline = [[] for _ in range(9)]
+p = set([(sx, sy)])
+visited = [set([*p]), set()]
+visited_per_region = [[set(), set()] for _ in range(9)]
+
+steps = 0
+while True:
+    np = set()
+    for x, y in p:
+        for d in range(4):
+            nx = x + (d == 0) - (d == 1)
+            ny = y + (d == 2) - (d == 3)
+
+            if nx < -w or nx >= 2 * w or ny < -h or ny >= 2 * h:
+                continue
+
+            key = (nx, ny)
+            if g[ny % h][nx % w]:
+                continue
+
+            v = visited[steps % 2]
+            if key in v:
+                continue
+            v.add(key)
+
+            for idx in range(9):
+                if -w <= nx - (idx % 3) * w < 0 and -h <= ny - (idx // 3) * h < 0:
+                    visited_per_region[idx][steps % 2].add(key)
+
+            np.add(key)
+
+    p = np
+    steps += 1
+
+    for idx in range(9):
+        c = len(visited_per_region[idx][steps % 2])
+        timeline[idx].append(c)
+
+    if len(timeline[0]) >= 3:
+        for idx in range(9):
+            if timeline[idx][-1] != timeline[idx][-3]:
+                break
+        else:
+            break
+
+    if steps % 100 == 0:
+        print(f"steps = {steps}")
+
+def take(t, idx):
+    if idx >= len(t):
+        idx -= (idx - len(t) + 2) // 2 * 2
+    return t[idx]
+
+N = 26501365
+
+# Mid
+t = timeline[4]
+mid = take(t, N)
+
+# Corners
+corners = 0
+for idx in [0, 2, 6, 8]:
+    t = timeline[idx]
+
+    one = t.index(1)
+    a, b = divmod(N - one, w)
+    a += 1
+
+    # Shell
+    for inward in range(2):
+        i = one + b + inward * w
+        corners += take(t, i) * max(a - inward, 0)
+
+    # Interior
+    if a > 0:
+        x = max(a, 0)
+        p = max((x - 1) * (x - 2) // 2, 0)
+
+        corners += p * take(t, N + 1)
+
+# Axes
+axes = 0
+for idx in [1, 3, 5, 7]:
+    t = timeline[idx]
+
+    one = t.index(1)
+    a, b = divmod(N - one, w)
+    a += 1
+
+    # Shell
+    for inward in range(2):
+        if a - inward <= 0:
+            continue
+        i = one + b + inward * w
+        axes += take(t, i)
+
+    # Interior
+    p = max(a - 2, 0)
+    axes += p * take(t, N)
+
+print(mid + corners + axes)
