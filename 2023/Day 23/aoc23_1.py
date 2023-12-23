7517bdf3092fc07980afd06ec765b953255a5e6c
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-12-23 12:45:18 +0100

    Day 23

--- 2023/Day 23/aoc23_1.py
+++ 2023/Day 23/aoc23_1.py
@@ -1,4 +1,35 @@
 file = open("aoc23_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+w, h = len(g[0]), len(g)
+
+sx = 1
+sy = 0
+
+b = 0
+
+q = [(0, sx, sy, -1, -1)]
+while q:
+    c, x, y, fx, fy = q.pop()
+
+    if y == h - 1:
+        b = max(b, c)
+        continue
+
+    for d in range(4):
+        idx = "<>^v".find(g[y][x])
+        if idx != -1 and d != idx:
+            continue
+
+        nx = x - (d == 0) + (d == 1)
+        ny = y - (d == 2) + (d == 3)
+
+        if g[ny][nx] == "#":
+            continue
+        if ny == fy and nx == fx:
+            continue
+
+        q.append((c + 1, nx, ny, x, y))
+
+print(b)
--- 2023/Day 23/aoc23_2.py
+++ 2023/Day 23/aoc23_2.py
@@ -1,4 +1,73 @@
 file = open("aoc23_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+w, h = len(g[0]), len(g)
+
+nodes = [(1, 0), (w - 2, h - 1)]
+for iy in range(1, h - 1):
+    for ix in range(1, w - 1):
+        if g[iy][ix] == "#":
+            continue
+
+        c = 0
+        for d in range(4):
+            x = ix - (d == 0) + (d == 1)
+            y = iy - (d == 2) + (d == 3)
+            c += g[y][x] != "#"
+
+        if c >= 3:
+            nodes.append((ix, iy))
+
+neighbors = {}
+for a in nodes:
+    x, y = a
+
+    q = [(0, x, y, -1, -1)]
+
+    while q:
+        c, x, y, fx, fy = q.pop()
+
+        b = (x, y)
+        if b in nodes and a != b:
+            neighbors[a] = neighbors.get(a, {})
+            neighbors[a][b] = c
+            continue
+
+        for d in range(4):
+            nx = x - (d == 0) + (d == 1)
+            ny = y - (d == 2) + (d == 3)
+
+            if nx < 0 or nx > w - 1 or ny < 0 or ny > h - 1:
+                continue
+
+            if g[ny][nx] == "#":
+                continue
+            if ny == fy and nx == fx:
+                continue
+
+            q.append((c + 1, nx, ny, x, y))
+
+b = 0
+
+q = [(0, 1, 0, 0)]
+while q:
+    c, x, y, seen = q.pop()
+
+    if x == w - 2 and y == h - 1:
+        if b < c:
+            b = c
+            print(f"{b} (?)")
+        continue
+
+    idx = nodes.index((x, y))
+    seen |= 1 << idx
+
+    for n, cost in neighbors[(x, y)].items():
+        idx = nodes.index(n)
+        if (seen >> idx) & 1:
+            continue
+
+        q.append((c + cost, n[0], n[1], seen))
+
+print(b)
