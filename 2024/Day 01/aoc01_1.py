fc8db7a768e1b853ef4beaa182bbff23812f988a
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-01 10:24:28 +0100

    Day 01

--- 2024/Day 01/aoc01_1.py
+++ 2024/Day 01/aoc01_1.py
@@ -2,3 +2,18 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+a = []
+b = []
+for x in l:
+    p, q = [int(j) for j in x.split()]
+    a.append(p)
+    b.append(q)
+
+a = sorted(a)
+b = sorted(b)
+
+s = 0
+for p, q in zip(a, b):
+    s += abs(p - q)
+
+print(s)
--- 2024/Day 01/aoc01_2.py
+++ 2024/Day 01/aoc01_2.py
@@ -2,3 +2,15 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+a = {}
+b = {}
+for x in l:
+    p, q = [int(j) for j in x.split()]
+    a[p] = a.get(p, 0) + 1
+    b[q] = b.get(q, 0) + 1
+
+s = 0
+for p in a:
+    s += p * a.get(p, 0) * b.get(p, 0)
+
+print(s)
