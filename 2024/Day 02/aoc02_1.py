d0c4bede32e60df519b8609d5a5dfa5d39c73bd9
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-02 11:31:06 +0100

    Day 02

--- 2024/Day 02/aoc02_1.py
+++ 2024/Day 02/aoc02_1.py
@@ -1,4 +1,16 @@
 file = open("aoc02_input.txt")
-l = file.read().split("\n")
+g = [[int(j) for j in row.split()] for row in file.read().split("\n")]
 file.close()
 
+x = 0
+for row in g:
+    if row == sorted(row) or row == sorted(row)[::-1]:
+        for i in range(len(row) - 1):
+            if abs(row[i] - row[i + 1]) > 3:
+                break
+            if abs(row[i] - row[i + 1]) == 0:
+                break
+        else:
+            x += 1
+
+print(x)
--- 2024/Day 02/aoc02_2.py
+++ 2024/Day 02/aoc02_2.py
@@ -1,4 +1,20 @@
 file = open("aoc02_input.txt")
-l = file.read().split("\n")
+g = [[int(j) for j in row.split()] for row in file.read().split("\n")]
 file.close()
 
+x = 0
+for l in g:
+    pos = 0
+    for i in range(len(l)):
+        row = l[:i] + l[i+1:]
+        if row == sorted(row) or row == sorted(row)[::-1]:
+            for i in range(len(row) - 1):
+                if abs(row[i] - row[i + 1]) > 3:
+                    break
+                if abs(row[i] - row[i + 1]) == 0:
+                    break
+            else:
+                pos = 1
+    x += pos
+
+print(x)
