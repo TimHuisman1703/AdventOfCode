f92518fcfbdacc750c886df187de3128039e404f
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-03 13:22:33 +0100

    Day 03

--- 2024/Day 03/aoc03_1.py
+++ 2024/Day 03/aoc03_1.py
@@ -1,4 +1,26 @@
 file = open("aoc03_input.txt")
-l = file.read().split("\n")
+s = file.read()
 file.close()
 
+idx = -1
+
+def mul(x, y):
+    return x * y
+
+r = 0
+while idx < len(s):
+    idx = s.find("mul(", idx + 1)
+    if idx == -1:
+        break
+    
+    end = s.find(")", idx)
+    if end == -1:
+        break
+    expr = s[idx:end + 1]
+
+    try:
+        r += eval(expr)
+    except:
+        pass
+
+print(r)
--- 2024/Day 03/aoc03_2.py
+++ 2024/Day 03/aoc03_2.py
@@ -1,4 +1,39 @@
 file = open("aoc03_input.txt")
-l = file.read().split("\n")
+s = file.read()
 file.close()
 
+idx = -1
+
+def mul(x, y):
+    return x * y
+
+r = 0
+enabled = True
+while idx < len(s):
+    do_idx = s.find("do()", idx + 1) % (len(s) + 1)
+    dont_idx = s.find("don't()", idx + 1) % (len(s) + 1)
+    idx = s.find("mul(", idx + 1) % (len(s) + 1)
+    if idx == -1:
+        break
+
+    if do_idx < idx and do_idx < dont_idx:
+        enabled = True
+        idx = do_idx
+        continue
+    if dont_idx < idx:
+        enabled = False
+        idx = dont_idx
+        continue
+    
+    end = s.find(")", idx)
+    if end == -1:
+        break
+    expr = s[idx:end + 1]
+
+    try:
+        if enabled:
+            r += eval(expr)
+    except:
+        pass
+
+print(r)
