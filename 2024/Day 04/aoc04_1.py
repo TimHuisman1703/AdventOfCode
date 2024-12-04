048131ff044299e59c215901558fdb9fe3b42c5f
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-04 12:45:16 +0100

    Day 04

--- 2024/Day 04/aoc04_1.py
+++ 2024/Day 04/aoc04_1.py
@@ -1,4 +1,23 @@
 file = open("aoc04_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+xmas = "XMAS"
+
+r = 0
+for dy in range(-1, 2):
+    for dx in range(-1, 2):
+        for iy in range(len(g)):
+            for ix in range(len(g[0])):
+                x, y = ix, iy
+                for i in range(4):
+                    if x < 0 or x >= len(g[0]) or y < 0 or y >= len(g):
+                        break
+                    if g[y][x] != xmas[i]:
+                        break
+                    y += dy
+                    x += dx
+                else:
+                    r += 1
+
+print(r)
--- 2024/Day 04/aoc04_2.py
+++ 2024/Day 04/aoc04_2.py
@@ -1,4 +1,17 @@
 file = open("aoc04_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+r = 0
+for iy in range(1, len(g) - 1):
+    for ix in range(1, len(g[0])- 1):
+        if g[iy][ix] == "A":
+            l = [
+                g[iy - 1][ix - 1],
+                g[iy - 1][ix + 1],
+                g[iy + 1][ix + 1],
+                g[iy + 1][ix - 1],
+            ]
+            r += "".join(l) in ["MSSM", "SMMS", "MMSS", "SSMM"]
+
+print(r)
