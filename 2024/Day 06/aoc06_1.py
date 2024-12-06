d8f4fa5e71866ae844575b20b6c7e14dd83fd6c7
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-06 06:12:46 +0100

    Day 06

--- 2024/Day 06/aoc06_1.py
+++ 2024/Day 06/aoc06_1.py
@@ -2,3 +2,29 @@ file = open("aoc06_input.txt")
 l = file.read().split("\n")
 file.close()
 
+g = [[x == "#" for x in row] for row in l]
+
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if l[iy][ix] == "^":
+            x = ix
+            y = iy
+
+seen = set()
+dx = 0
+dy = -1
+while True:
+    seen.add((x, y))
+
+    nx = x + dx
+    ny = y + dy
+    if nx < 0 or nx >= len(g[0]) or ny < 0 or ny >= len(g):
+        break
+
+    if g[ny][nx]:
+        dx, dy = -dy, dx
+        continue
+
+    x, y = nx, ny
+
+print(len(seen))
--- 2024/Day 06/aoc06_2.py
+++ 2024/Day 06/aoc06_2.py
@@ -2,3 +2,44 @@ file = open("aoc06_input.txt")
 l = file.read().split("\n")
 file.close()
 
+g = [[x == "#" for x in row] for row in l]
+
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if l[iy][ix] == "^":
+            sx = ix
+            sy = iy
+
+r = 0
+
+for by in range(len(g)):
+    print(f"{by=}")
+    for bx in range(len(g[0])):
+        if g[by][bx]:
+            continue
+        g[by][bx] = True
+
+        seen = set()
+        dx = 0
+        dy = -1
+        x, y = sx, sy
+        while True:
+            if (x, y, dx, dy) in seen:
+                r += 1
+                break
+            seen.add((x, y, dx, dy))
+
+            nx = x + dx
+            ny = y + dy
+            if nx < 0 or nx >= len(g[0]) or ny < 0 or ny >= len(g):
+                break
+
+            if g[ny][nx]:
+                dx, dy = -dy, dx
+                continue
+
+            x, y = nx, ny
+
+        g[by][bx] = False
+
+print(r)
