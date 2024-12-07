e4423e1422d5866189a95812babedb072508641a
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-07 15:57:19 +0100

    Day 07

--- 2024/Day 07/aoc07_1.py
+++ 2024/Day 07/aoc07_1.py
@@ -2,3 +2,23 @@ file = open("aoc07_input.txt")
 l = file.read().split("\n")
 file.close()
 
+r = 0
+for w in l:
+    t, w = w.split(": ")
+    t = int(t)
+    w = [int(j) for j in w.split()]
+
+    pos = False
+    for i in range(1 << (len(w) - 1)):
+        c = w[0]
+        for j in range(1, len(w)):
+            if i >> (j - 1) & 1:
+                c *= w[j]
+            else:
+                c += w[j]
+        
+        if c == t:
+            r += c
+            break
+
+print(r)
--- 2024/Day 07/aoc07_2.py
+++ 2024/Day 07/aoc07_2.py
@@ -2,3 +2,31 @@ file = open("aoc07_input.txt")
 l = file.read().split("\n")
 file.close()
 
+r = 0
+for idx, w in enumerate(l):
+    print(f"{idx=}")
+
+    t, w = w.split(": ")
+    t = int(t)
+    w = [int(j) for j in w.split()]
+
+    pos = False
+    for i in range(3 ** (len(w) - 1)):
+        c = w[0]
+        for j in range(1, len(w)):
+            ty = i // (3 ** (j - 1)) % 3
+            if ty == 0:
+                c *= w[j]
+            elif ty == 1:
+                c += w[j]
+            else:
+                c = int(str(c) + str(w[j]))
+            
+            if c > t:
+                break
+        
+        if c == t:
+            r += c
+            break
+
+print(r)
