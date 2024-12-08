44b47623fe215c999a9babcd140c27669cf97bfb
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-08 10:21:19 +0100

    Day 08

--- 2024/Day 08/aoc08_1.py
+++ 2024/Day 08/aoc08_1.py
@@ -2,3 +2,26 @@ file = open("aoc08_input.txt")
 l = file.read().split("\n")
 file.close()
 
+d = {}
+for iy in range(len(l)):
+    for ix in range(len(l[iy])):
+        c = l[iy][ix]
+        if c != ".":
+            if c not in d:
+                d[c] = []
+            d[c].append((ix, iy))
+
+seen = set()
+for c in d:
+    dc = d[c]
+    for i in range(len(dc)):
+        for j in range(i + 1, len(dc)):
+            seen.add(tuple(2 * dc[i][k] - dc[j][k] for k in range(2)))
+            seen.add(tuple(2 * dc[j][k] - dc[i][k] for k in range(2)))
+
+r = 0
+for x, y in seen:
+    if x >= 0 and x < len(l[0]) and y >= 0 and y < len(l):
+        r += 1
+
+print(r)
--- 2024/Day 08/aoc08_2.py
+++ 2024/Day 08/aoc08_2.py
@@ -2,3 +2,32 @@ file = open("aoc08_input.txt")
 l = file.read().split("\n")
 file.close()
 
+d = {}
+for iy in range(len(l)):
+    for ix in range(len(l[iy])):
+        c = l[iy][ix]
+        if c != ".":
+            if c not in d:
+                d[c] = []
+            d[c].append((ix, iy))
+
+seen = set()
+for c in d:
+    dc = d[c]
+    for i in range(len(dc)):
+        for j in range(i + 1, len(dc)):
+            for _ in range(2):
+                x, y = [dc[i][k] for k in range(2)]
+                dx, dy = [dc[j][k] - dc[i][k] for k in range(2)]
+
+                while True:
+                    x += dx
+                    y += dy
+                    if not (x >= 0 and x < len(l[0]) and y >= 0 and y < len(l)):
+                        break
+
+                    seen.add((y, x))
+
+                i, j = j, i
+
+print(len(seen))
