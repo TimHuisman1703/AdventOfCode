1e4f8ddf56b7b413c2955c58c55756efb8698e00
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-09 09:50:24 +0100

    Day 09

--- 2024/Day 09/aoc09_1.py
+++ 2024/Day 09/aoc09_1.py
@@ -2,3 +2,31 @@ file = open("aoc09_input.txt")
 l = file.read().split("\n")
 file.close()
 
+r = []
+idx = 1
+for c in l[0]:
+    c = int(c)
+    r.extend(c * [max(0, idx)])
+
+    if idx > 0:
+        idx = -idx
+    else:
+        idx = -idx + 1
+
+idx = 0
+while idx < len(r):
+    if r[idx] == 0:
+        if r[-1] == 0:
+            r.pop()
+        else:
+            r[idx] = r.pop()
+            idx += 1
+    else:
+        idx += 1
+
+s = 0
+for i, c in enumerate(r):
+    if r[i]:
+        s += i * (c - 1)
+
+print(s)
--- 2024/Day 09/aoc09_2.py
+++ 2024/Day 09/aoc09_2.py
@@ -2,3 +2,49 @@ file = open("aoc09_input.txt")
 l = file.read().split("\n")
 file.close()
 
+import heapq
+
+b = [[] for _ in range(10)]
+q = []
+
+s = 0
+idx = 1
+sc = 0
+st = []
+cs = []
+for c in l[0]:
+    c = int(c)
+
+    if idx > 0:
+        st.append(sc)
+        q.append((idx, c))
+        cs.append(c)
+        idx = -idx
+    else:
+        b[c].append((len(q) - 1, sc))
+        idx = -idx + 1
+
+    sc += c
+
+for i in range(len(q) - 1, -1, -1):
+    idx, c = q.pop()
+
+    if idx == 0:
+        continue
+
+    best = (10 ** 10, -1)
+    for p in range(c, 10):
+        if b[p] and b[p][0][1] < best[0] and b[p][0][0] < i:
+            best = (b[p][0][1], p)
+    
+    if best[0] < 10 ** 10:
+        j, x = heapq.heappop(b[best[1]])
+        st[idx - 1] = best[0]
+        heapq.heappush(b[best[1] - c], (j, x + c))
+
+s = 0
+for idx, t in enumerate(st):
+    for j in range(t, t + cs[idx]):
+        s += j * idx
+
+print(s)
