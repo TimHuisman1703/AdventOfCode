a28587b006ac5a4da6f107a1bd3478dd935f4831
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-10 08:10:19 +0100

    Day 10

--- 2024/Day 10/aoc10_1.py
+++ 2024/Day 10/aoc10_1.py
@@ -1,4 +1,37 @@
 file = open("aoc10_input.txt")
-l = file.read().split("\n")
+g = [[int(j) for j in row] for row in file.read().split("\n")]
 file.close()
 
+r = 0
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if g[iy][ix]:
+            continue
+
+        q = [(ix, iy)]
+        visited = set()
+        
+        s = 0
+        while q:
+            x, y = q.pop()
+
+            if (x, y) in visited:
+                continue
+            visited.add((x, y))
+
+            if g[y][x] == 9:
+                r += 1
+                continue
+
+            for d in range(4):
+                nx = x + (d == 0) - (d == 1)
+                ny = y + (d == 2) - (d == 3)
+
+                if nx < 0 or nx >= len(g[0]) or ny < 0 or ny >= len(g):
+                    continue
+                if g[y][x] + 1 != g[ny][nx]:
+                    continue
+
+                q.append((nx, ny))
+
+print(r)
