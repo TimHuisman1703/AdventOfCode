1334e1ef27da448a0f4323246105d420b5d16f91
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-11 15:46:56 +0100

    Day 11

--- 2024/Day 11/aoc11_2.py
+++ 2024/Day 11/aoc11_2.py
@@ -1,4 +1,25 @@
 file = open("aoc11_input.txt")
-l = file.read().split("\n")
+l = [int(j) for j in file.read().split()]
 file.close()
 
+ITERATIONS = 75
+
+d = {c: l.count(c) for c in set(l)}
+
+for _ in range(ITERATIONS):
+    nd = {}
+    for c, x in d.items():
+        if c == 0:
+            nd[1] = nd.get(1, 0) + x
+        elif len(str(c)) % 2 == 0:
+            a, b = str(c)[:len(str(c)) // 2], str(c)[len(str(c)) // 2:]
+            a = int(a)
+            b = int(b)
+            nd[a] = nd.get(a, 0) + x
+            nd[b] = nd.get(b, 0) + x
+        else:
+            nd[2024 * c] = nd.get(2024 * c, 0) + x
+
+    d = nd
+
+print(sum(d.values()))
