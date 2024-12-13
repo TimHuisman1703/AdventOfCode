46e70162f54a0f1ef1f26b6efabfbadf2b9f2c84
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-13 08:58:11 +0100

    Day 13

--- 2024/Day 13/aoc13_1.py
+++ 2024/Day 13/aoc13_1.py
@@ -1,4 +1,21 @@
 file = open("aoc13_input.txt")
-l = file.read().split("\n")
+l = file.read().split("\n\n")
 file.close()
 
+r = 0
+for w in l:
+    w = w.split("\n")
+    ax, ay = [int(j[2:]) for j in w[0].split(": ")[1].split(", ")]
+    bx, by = [int(j[2:]) for j in w[1].split(": ")[1].split(", ")]
+    tx, ty = [int(j[2:]) for j in w[2].split(": ")[1].split(", ")]
+
+    i = 0
+    while ax * i <= tx and ay * i <= ty:
+        if (tx - ax * i) % bx == 0 and (ty - ay * i) % by == 0 and (tx - ax * i) // bx == (ty - ay * i) // by:
+            j = (tx - ax * i) // bx
+            r += i * 3 + j
+            break
+
+        i += 1
+
+print(r)
--- 2024/Day 13/aoc13_2.py
+++ 2024/Day 13/aoc13_2.py
@@ -1,4 +1,36 @@
 file = open("aoc13_input.txt")
-l = file.read().split("\n")
+l = file.read().split("\n\n")
 file.close()
 
+r = 0
+for w in l:
+    w = w.split("\n")
+    ax, ay = [int(j[2:]) for j in w[0].split(": ")[1].split(", ")]
+    bx, by = [int(j[2:]) for j in w[1].split(": ")[1].split(", ")]
+    tx, ty = [int(j[2:]) + 10000000000000 for j in w[2].split(": ")[1].split(", ")]
+
+    # y = x * (ay / ax)
+    # (y - ty) = (x - tx) * (by / bx)
+    #
+    # y = x * (by / bx) - tx * (by / bx) + ty = x * (ay / ax)
+    # x * (by / bx - ay / ax) - tx * (by / bx) + ty = 0
+    # x * (by / bx - ay / ax) = tx * (by / bx) - ty
+    # x = (tx * (by / bx) - ty) / (by / bx - ay / ax)
+    # x = (tx * ax * by - ty * ax * bx) / (ax * by - bx * ay)
+
+    num = tx * ax * by - ty * ax * bx
+    den = ax * by - bx * ay
+    if num % den != 0:
+        continue
+
+    x = num // den
+    if x % ax != 0:
+        continue
+
+    pa = x // ax
+    pbx = (tx - ax * pa) // bx
+    pby = (ty - ay * pa) // by
+    if pbx == pby:
+        r += 3 * pa + pbx
+
+print(r)
