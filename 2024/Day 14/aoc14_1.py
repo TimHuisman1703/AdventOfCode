5c734926a2149b706f5209565976a9ea363945de
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-14 12:30:13 +0100

    Day 14

--- 2024/Day 14/aoc14_1.py
+++ 2024/Day 14/aoc14_1.py
@@ -2,3 +2,22 @@ file = open("aoc14_input.txt")
 l = file.read().split("\n")
 file.close()
 
+c = [0, 0, 0, 0]
+
+for w in l:
+    pw, vw = w.split()
+    px, py = [int(j) for j in pw[2:].split(",")]
+    vx, vy = [int(j) for j in vw[2:].split(",")]
+
+    for _ in range(100):
+        px = (px + vx) % 101
+        py = (py + vy) % 103
+    
+    if px != 50 and py != 51:
+        c[(px < 50) + 2 * (py < 51)] += 1
+
+p = 1
+for x in c:
+    p *= x
+
+print(p)
--- 2024/Day 14/aoc14_2.py
+++ 2024/Day 14/aoc14_2.py
@@ -2,3 +2,27 @@ file = open("aoc14_input.txt")
 l = file.read().split("\n")
 file.close()
 
+ps = []
+vs = []
+for w in l:
+    pw, vw = w.split()
+    ps.append([int(j) for j in pw[2:].split(",")])
+    vs.append([int(j) for j in vw[2:].split(",")])
+
+t = 0
+while True:
+    t += 1
+
+    for i in range(len(ps)):
+        ps[i][0] = (ps[i][0] + vs[i][0]) % 101
+        ps[i][1] = (ps[i][1] + vs[i][1]) % 103
+
+    g = [["." for _ in range(101)] for _ in range(103)]
+    for x, y in ps:
+        g[y][x] = "#"
+
+    g = "\n".join("".join(row) for row in g)
+    if "#" * 10 in g:
+        print(g)
+        print(t)
+        break
