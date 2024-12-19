73e5a6395e7c791ac3ef846ea4accf9e164c0cd9
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-19 13:08:03 +0100

    Day 19

--- 2024/Day 19/aoc19_1.py
+++ 2024/Day 19/aoc19_1.py
@@ -2,3 +2,20 @@ file = open("aoc19_input.txt")
 l = file.read().split("\n")
 file.close()
 
+ps = set(l[0].split(", "))
+
+r = 0
+for w in l[2:]:
+    pos = [False] * (len(w) + 1)
+    pos[0] = True
+
+    for i in range(1, len(w) + 1):
+        for j in range(i):
+            if pos[j]:
+                if w[j:i] in ps:
+                    pos[i] = True
+                    break
+
+    r += pos[-1]
+
+print(r)
--- 2024/Day 19/aoc19_2.py
+++ 2024/Day 19/aoc19_2.py
@@ -2,3 +2,19 @@ file = open("aoc19_input.txt")
 l = file.read().split("\n")
 file.close()
 
+ps = set(l[0].split(", "))
+
+r = 0
+for w in l[2:]:
+    pos = [0] * (len(w) + 1)
+    pos[0] = 1
+
+    for i in range(1, len(w) + 1):
+        for j in range(i):
+            if pos[j]:
+                if w[j:i] in ps:
+                    pos[i] += pos[j]
+
+    r += pos[-1]
+
+print(r)
