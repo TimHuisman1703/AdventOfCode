c9c4852d6e69dbbcea89655965d42fe1cdb9c370
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-21 00:40:38 +0100

    Day 20

--- 2024/Day 20/aoc20_1.py
+++ 2024/Day 20/aoc20_1.py
@@ -1,4 +1,66 @@
 file = open("aoc20_input.txt")
-l = file.read().split("\n")
+g = [list(row) for row in file.read().split("\n")]
 file.close()
 
+import heapq
+
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if g[iy][ix] == "S":
+            sx, sy = ix, iy
+        if g[iy][ix] == "E":
+            ex, ey = ix, iy
+
+normal_cost = 0
+    
+q = [(0, sx, sy)]
+visited = set()
+while q:
+    cost, x, y = heapq.heappop(q)
+
+    if x == ex and y == ey:
+        normal_cost = cost
+        break
+
+    key = (x, y)
+    if key in visited:
+        continue
+    visited.add(key)
+
+    for d in range(4):
+        nx = x + (d == 0) - (d == 1)
+        ny = y + (d == 2) - (d == 3)
+        if g[ny][nx] != "#":
+            heapq.heappush(q, (cost + 1, nx, ny))
+
+r = 0
+for by in range(1, len(g) - 1):
+    print(f"{by=}")
+    for bx in range(1, len(g[0]) - 1):
+        if g[by][bx] == "#":
+            g[by][bx] = "."
+    
+            q = [(0, sx, sy)]
+            visited = set()
+            while q:
+                cost, x, y = heapq.heappop(q)
+
+                if x == ex and y == ey:
+                    if cost + 100 <= normal_cost:
+                        r += 1
+                    break
+
+                key = (x, y)
+                if key in visited:
+                    continue
+                visited.add(key)
+
+                for d in range(4):
+                    nx = x + (d == 0) - (d == 1)
+                    ny = y + (d == 2) - (d == 3)
+                    if g[ny][nx] != "#":
+                        heapq.heappush(q, (cost + 1, nx, ny))
+            
+            g[by][bx] = "#"
+
+print(r)
--- 2024/Day 20/aoc20_2.py
+++ 2024/Day 20/aoc20_2.py
@@ -1,4 +1,75 @@
 file = open("aoc20_input.txt")
-l = file.read().split("\n")
+g = [list(row) for row in file.read().split("\n")]
 file.close()
 
+SIZE = 20
+GAIN = 100
+
+import heapq
+
+for iy in range(len(g)):
+    for ix in range(len(g[0])):
+        if g[iy][ix] == "S":
+            sx, sy = ix, iy
+        if g[iy][ix] == "E":
+            ex, ey = ix, iy
+
+q = [(0, sx, sy)]
+ds = [[10 ** 10 for _ in range(len(g[0]))] for _ in range(len(g))]
+normal_cost = 0
+while q:
+    cost, x, y = heapq.heappop(q)
+
+    key = (x, y)
+    if ds[y][x] < 10 ** 10:
+        continue
+    ds[y][x] = cost
+
+    if (x, y) == (ex, ey):
+        normal_cost = cost
+
+    for d in range(4):
+        nx = x + (d == 0) - (d == 1)
+        ny = y + (d == 2) - (d == 3)
+        if g[ny][nx] != "#":
+            heapq.heappush(q, (cost + 1, nx, ny))
+
+q = [(0, ex, ey)]
+de = [[10 ** 10 for _ in range(len(g[0]))] for _ in range(len(g))]
+while q:
+    cost, x, y = heapq.heappop(q)
+
+    key = (x, y)
+    if de[y][x] < 10 ** 10:
+        continue
+    de[y][x] = cost
+
+    for d in range(4):
+        nx = x + (d == 0) - (d == 1)
+        ny = y + (d == 2) - (d == 3)
+        if g[ny][nx] != "#":
+            heapq.heappush(q, (cost + 1, nx, ny))
+
+r = 0
+test = {}
+for iy in range(len(g)):
+    print(f"{iy=}")
+    for ix in range(len(g[0])):
+        if g[iy][ix] != "#":
+            for dy in range(-SIZE, SIZE + 1):
+                for dx in range(-SIZE, SIZE + 1):
+                    dist = abs(dx) + abs(dy)
+                    if dist > SIZE:
+                        continue
+
+                    jx, jy = ix + dx, iy + dy
+                    if jx < 0 or jx >= len(g[0]) or jy < 0 or jy >= len(g):
+                        continue
+
+                    cost = ds[iy][ix] + dist + de[jy][jx]
+                    gain = normal_cost - cost
+                    if gain >= GAIN:
+                        test[gain] = test.get(gain, 0) + 1
+                        r += 1
+
+print(r)
