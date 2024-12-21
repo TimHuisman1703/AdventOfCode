63fbbfd51c38eb59130b9f7e18fd98967b2e4753
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-21 09:09:11 +0100

    Day 21

--- 2024/Day 21/aoc21_1.py
+++ 2024/Day 21/aoc21_1.py
@@ -2,3 +2,68 @@ file = open("aoc21_input.txt")
 l = file.read().split("\n")
 file.close()
 
+import heapq
+
+pads = [j.split("\n") for j in ["789\n456\n123\n 0A", " ^A\n<V>"]]
+
+ds = [{}, {}]
+for i, p in enumerate(pads):
+    for iy in range(len(p)):
+        for ix in range(len(p[0])):
+            ds[i][p[iy][ix]] = (ix, iy)
+pad0, pad1 = pads
+
+r = 0
+for w in l:
+    d0, d1 = ds
+
+    q = [(0, 0, *d0["A"], *d1["A"], *d1["A"], None)]
+    visited = {}
+    while q:
+        cost, idx, ax, ay, bx, by, cx, cy, prev = heapq.heappop(q)
+
+        key = (idx, ax, ay, bx, by, cx, cy)
+        if key in visited:
+            continue
+        visited[key] = (cost, prev)
+
+        if idx == len(w):
+            break
+
+        for d in range(4):
+            nbx = bx + (d == 0) - (d == 1)
+            nby = by + (d == 2) - (d == 3)
+            if nbx < 0 or nbx >= len(pad1[0]) or nby < 0 or nby >= len(pad1):
+                continue
+            if pad1[nby][nbx] == " ":
+                continue
+
+            ncx, ncy = d1["><V^"[d]]
+            new_cost = cost + abs(cx - ncx) + abs(cy - ncy) + 1
+            heapq.heappush(q, (new_cost, idx, ax, ay, nbx, nby, ncx, ncy, key))
+
+        for d in range(4):
+            if pad1[by][bx] != "><V^"[d]:
+                continue
+            nax = ax + (d == 0) - (d == 1)
+            nay = ay + (d == 2) - (d == 3)
+            if nax < 0 or nax >= len(pad0[0]) or nay < 0 or nay >= len(pad0):
+                continue
+            if pad0[nay][nax] == " ":
+                continue
+            ncx, ncy = d1["A"]
+            new_cost = cost + abs(cx - ncx) + abs(cy - ncy) + 1
+            heapq.heappush(q, (new_cost, idx, nax, nay, bx, by, ncx, ncy, key))
+
+        if pad1[by][bx] == "A":
+            if pad0[ay][ax] == w[idx]:
+                ncx, ncy = d1["A"]
+                new_cost = cost + abs(cx - ncx) + abs(cy - ncy) + 1
+                heapq.heappush(q, (new_cost, idx + 1, ax, ay, bx, by, ncx, ncy, key))
+
+    w = w[:-1]
+    while w[0] == "0":
+        w = w[1:]
+    r += cost * int(w)
+
+print(r)
--- 2024/Day 21/aoc21_2.py
+++ 2024/Day 21/aoc21_2.py
@@ -2,3 +2,87 @@ file = open("aoc21_input.txt")
 l = file.read().split("\n")
 file.close()
 
+import heapq
+
+LAYERS = 2 + 23
+
+pad = " ^A\n<V>".split("\n")
+
+# > < V ^
+dist = {(frc, toc): 1 for frc in "A><V^" for toc in "A><V^"}
+positions = [(2, 0), (2, 1), (0, 1), (1, 1), (1, 0)]
+for _ in range(LAYERS):
+    dist_new = {}
+    for fr in range(5):
+        frc = "A><V^"[fr]
+        for to in range(5):
+            toc = "A><V^"[to]
+            tox, toy = positions[to]
+
+            if fr == to:
+                dist_new[(frc, toc)] = 1
+                continue
+
+            q = [(0, *positions[fr], "A")]
+            visited = set()
+            while q:
+                cost, x, y, p = heapq.heappop(q)
+
+                key = (x, y, p)
+                if key in visited:
+                    continue
+                visited.add(key)
+
+                if x == tox and y == toy and p == "A":
+                    dist_new[(frc, toc)] = cost
+                    break
+
+                for np in "A><V^":
+                    nx = x + (np == ">") - (np == "<")
+                    ny = y + (np == "V") - (np == "^")
+                    if (nx, ny) not in positions:
+                        continue
+                    new_cost = cost + dist[(p, np)]
+                    heapq.heappush(q, (new_cost, nx, ny, np))
+    dist = dist_new
+
+pad = "789\n456\n123\n 0A".split("\n")
+
+r = 0
+for w in l:
+    q = [(0, 0, 2, 3, "A", None)]
+    visited = set()
+    while q:
+        cost, idx, x, y, p, prev = heapq.heappop(q)
+
+        key = (idx, x, y, p)
+        if key in visited:
+            continue
+        visited.add(key)
+
+        if idx == len(w):
+            break
+        elif pad[y][x] == w[idx]:
+            np = "A"
+            new_cost = cost + dist[(p, np)]
+            heapq.heappush(q, (new_cost, idx + 1, x, y, np, key))
+            continue
+
+        for d in range(4):
+            nx = x + (d == 0) - (d == 1)
+            ny = y + (d == 2) - (d == 3)
+            if nx < 0 or nx >= len(pad[0]) or ny < 0 or ny >= len(pad):
+                continue
+            if pad[ny][nx] == " ":
+                continue
+
+            np = "><V^"[d]
+            new_cost = cost + dist[(p, np)]
+            heapq.heappush(q, (new_cost, idx, nx, ny, np, key))
+
+    w = w[:-1]
+    while w[0] == "0":
+        w = w[1:]
+    r += cost * int(w)
+
+print(r)
