2d4bceeecb01d43d115ff10f78a1594382fe8aaf
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-22 10:55:14 +0100

    Day 22

--- 2024/Day 22/aoc22_1.py
+++ 2024/Day 22/aoc22_1.py
@@ -2,3 +2,14 @@ file = open("aoc22_input.txt")
 l = file.read().split("\n")
 file.close()
 
+l = [int(j) for j in l]
+
+r = 0
+for x in l:
+    for _ in range(2000):
+        x = (x ^ (x * 64)) % 16777216
+        x = (x ^ (x // 32)) % 16777216
+        x = (x ^ (x * 2048)) % 16777216
+    r += x
+
+print(r)
--- 2024/Day 22/aoc22_2.py
+++ 2024/Day 22/aoc22_2.py
@@ -2,3 +2,29 @@ file = open("aoc22_input.txt")
 l = file.read().split("\n")
 file.close()
 
+l = [int(j) for j in l]
+
+d = {}
+for b in l:
+    seq = []
+    x = b
+    seen = set()
+    for i in range(2000):
+        y = x
+        x = (x ^ (x * 64)) % 16777216
+        x = (x ^ (x // 32)) % 16777216
+        x = (x ^ (x * 2048)) % 16777216
+
+        if i == 0:
+            seq = [x % 10]
+        else:
+            seq = seq + [x % 10 - y % 10]
+        if len(seq) > 4:
+            seq = seq[1:]
+        
+        key = tuple(seq)
+        if len(key) == 4 and key not in seen:
+            d[key] = d.get(key, 0) + (x % 10)
+            seen.add(key)
+
+print(max(d.values()))
