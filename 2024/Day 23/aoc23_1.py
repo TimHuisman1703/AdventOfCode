27bbc9ef6042ba3fa26b75839d1eda4da7baaaa9
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2024-12-23 06:18:39 +0100

    Day 23

--- 2024/Day 23/aoc23_1.py
+++ 2024/Day 23/aoc23_1.py
@@ -2,3 +2,21 @@ file = open("aoc23_input.txt")
 l = file.read().split("\n")
 file.close()
 
+neigh = {}
+for w in l:
+    a, b = sorted(w.split("-"))
+    if a not in neigh:
+        neigh[a] = set()
+    if b not in neigh:
+        neigh[b] = set()
+    neigh[a].add(b)
+
+r = 0
+for a in neigh:
+    for b in neigh[a]:
+        for c in neigh[a]:
+            if c in neigh[b]:
+                if a[0] == "t" or b[0] == "t" or c[0] == "t":
+                    r += 1
+
+print(r)
--- 2024/Day 23/aoc23_2.py
+++ 2024/Day 23/aoc23_2.py
@@ -2,3 +2,33 @@ file = open("aoc23_input.txt")
 l = file.read().split("\n")
 file.close()
 
+neigh = {}
+for w in l:
+    a, b = w.split("-")
+    if a not in neigh:
+        neigh[a] = set()
+    if b not in neigh:
+        neigh[b] = set()
+    neigh[a].add(b)
+    neigh[b].add(a)
+
+cl = [{key} for key in neigh.keys()]
+while True:
+    print(f"len(cl) = {len(cl)}")
+    ncl = []
+
+    for c in cl:
+        for n in neigh.keys():
+            for x in c:
+                if n not in neigh[x]:
+                    break
+                if n < x:
+                    break
+            else:
+                ncl.append(c | {n})
+
+    if not ncl:
+        break
+    cl = ncl
+
+print(*sorted(cl[0]), sep=",")
