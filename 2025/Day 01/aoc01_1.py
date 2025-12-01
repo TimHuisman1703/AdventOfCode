ea4906ba94cc0a93babb483e5408f41da5cac29f
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-01 09:35:26 +0100

    Day 01

--- 2025/Day 01/aoc01_1.py
+++ 2025/Day 01/aoc01_1.py
@@ -2,3 +2,17 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 50
+r = 0
+
+for i in l:
+    d, x = i[0], int(i[1:])
+
+    if d == "L":
+        x *= -1
+    s += x
+
+    s %= 100
+    r += s == 0
+
+print(r)
--- 2025/Day 01/aoc01_2.py
+++ 2025/Day 01/aoc01_2.py
@@ -2,3 +2,24 @@ file = open("aoc01_input.txt")
 l = file.read().split("\n")
 file.close()
 
+s = 50
+r = 0
+
+for i in l:
+    d, x = i[0], int(i[1:])
+
+    if d == "L":
+        x *= -1
+    s += x
+
+    while s < 0:
+        if s - x != 0:
+            r += 1
+        s += 100
+    while s > 100:
+        if s - x != 100:
+            r += 1
+        s -= 100
+    r += s % 100 == 0
+
+print(r)
