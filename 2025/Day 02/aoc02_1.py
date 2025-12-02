82d5413e70edf606835b256d1884fa17d1718861
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-02 10:03:55 +0100

    Day 02

--- 2025/Day 02/aoc02_1.py
+++ 2025/Day 02/aoc02_1.py
@@ -1,4 +1,17 @@
 file = open("aoc02_input.txt")
-l = file.read().split("\n")
+l = file.read().replace("\n", "")
 file.close()
 
+l = l.split(",")
+
+r = 0
+for i in l:
+    print(">", i)
+    a, b = [int(j) for j in i.split("-")]
+
+    for x in range(a, b + 1):
+        s = str(x)
+        if 2 * s[:len(s) // 2] == s:
+            r += x
+
+print(r)
--- 2025/Day 02/aoc02_2.py
+++ 2025/Day 02/aoc02_2.py
@@ -1,4 +1,19 @@
 file = open("aoc02_input.txt")
-l = file.read().split("\n")
+l = file.read().replace("\n", "")
 file.close()
 
+l = l.split(",")
+
+r = 0
+for i in l:
+    print(">", i)
+    a, b = [int(j) for j in i.split("-")]
+
+    for x in range(a, b + 1):
+        s = str(x)
+        for n in range(1, len(s) // 2 + 1):
+            if s[:n] * (len(s) // n) == s:
+                r += x
+                break
+
+print(r)
