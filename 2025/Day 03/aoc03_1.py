f20914d1bb39f834fb3ab39a8ee279a8b7865481
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-03 07:34:31 +0100

    Day 03

--- 2025/Day 03/aoc03_1.py
+++ 2025/Day 03/aoc03_1.py
@@ -2,3 +2,14 @@ file = open("aoc03_input.txt")
 l = file.read().split("\n")
 file.close()
 
+r = 0
+for v in l:
+    v = [int(j) for j in v]
+
+    a = max(v[:-1])
+    idx = v.index(a)
+    b = max(v[idx + 1:])
+
+    r += 10 * a + b
+
+print(r)
--- 2025/Day 03/aoc03_2.py
+++ 2025/Day 03/aoc03_2.py
@@ -2,3 +2,16 @@ file = open("aoc03_input.txt")
 l = file.read().split("\n")
 file.close()
 
+r = 0
+for v in l:
+    v = [int(j) for j in v]
+
+    # In hindsight, DP might've been a bit of an overkill :P
+    m = [[0] + [-10 ** 20] * 12 for _ in range(len(v) + 1)]
+    for i in range(len(v) - 1, -1, -1):
+        for k in range(1, 13):
+            m[i][k] = max(m[i + 1][k], 10 ** (k - 1) * v[i] + m[i + 1][k - 1])
+
+    r += max(max(row) for row in m)
+
+print(r)
