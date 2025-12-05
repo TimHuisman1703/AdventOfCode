f0c4b8c4b85636bf26d4551da00fb236cf35d6fe
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-05 10:19:20 +0100

    Day 05

--- 2025/Day 05/aoc05_1.py
+++ 2025/Day 05/aoc05_1.py
@@ -1,4 +1,21 @@
 file = open("aoc05_input.txt")
-l = file.read().split("\n")
+l = file.read()
 file.close()
 
+la, lb = l.split("\n\n")
+
+l = []
+for i in la.split():
+    a, b = [int(j) for j in i.split("-")]
+    l.append((a, b))
+
+r = 0
+for v in lb.split():
+    v = int(v)
+
+    for a, b in l:
+        if a <= v <= b:
+            r += 1
+            break
+
+print(r)
--- 2025/Day 05/aoc05_2.py
+++ 2025/Day 05/aoc05_2.py
@@ -1,4 +1,26 @@
 file = open("aoc05_input.txt")
-l = file.read().split("\n")
+l = file.read()
 file.close()
 
+la, _ = l.split("\n\n")
+
+l = []
+for i in la.split():
+    a, b = [int(j) for j in i.split("-")]
+    l.append((a, b))
+
+l = sorted(l)
+
+idx = 0
+while idx < len(l) - 1:
+    if l[idx][1] + 1 >= l[idx + 1][0]:
+        b = max(l[idx][1], l[idx + 1][1])
+        l = l[:idx] + [(l[idx][0], b)] + l[idx + 2:]
+    else:
+        idx += 1
+
+r = 0
+for a, b in l:
+    r += b + 1 - a
+
+print(r)
