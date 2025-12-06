3f398b4d3a6e1aa1842e86cc3859d496d649975a
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-06 17:28:48 +0100

    Day 06

--- 2025/Day 06/aoc06_1.py
+++ 2025/Day 06/aoc06_1.py
@@ -2,3 +2,13 @@ file = open("aoc06_input.txt")
 l = file.read().split("\n")
 file.close()
 
+l = [row.split() for row in l]
+
+r = 0
+for i in range(len(l[0])):
+    *vs, op = [row[i] for row in l]
+    vs = [v + op for v in vs]
+    s = "".join(vs)[:-1]
+    r += eval(s)
+
+print(r)
--- 2025/Day 06/aoc06_2.py
+++ 2025/Day 06/aoc06_2.py
@@ -2,3 +2,15 @@ file = open("aoc06_input.txt")
 l = file.read().split("\n")
 file.close()
 
+indices = [j for j in range(len(l[-1])) if l[-1][j] != " "] + [len(l[-1]) + 1]
+l = [[row[indices[j]:indices[j + 1] - 1] for j in range(len(indices) - 1)] for row in l]
+
+r = 0
+for i in range(len(l[0])):
+    *vs, op = [row[i] for row in l]
+    vs = ["".join([row[j] for row in vs]) for j in range(len(vs[0]))]
+    vs = [v + op for v in vs]
+    s = "".join(vs).replace(" ", "")[:-1]
+    r += eval(s)
+
+print(r)
