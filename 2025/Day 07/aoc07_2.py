c75814fce9ffa8302f6a5e2d99b6fc0fe4722bbe
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-07 09:45:26 +0100

    Day 07

--- 2025/Day 07/aoc07_2.py
+++ 2025/Day 07/aoc07_2.py
@@ -1,4 +1,31 @@
 file = open("aoc07_input.txt")
-l = file.read().split("\n")
+g = file.read().split("\n")
 file.close()
 
+import heapq
+
+x = g[0].find("S")
+y = 0
+
+q = [(y, x, 1)]
+r = 1
+
+while q:
+    y, x, c = heapq.heappop(q)
+
+    while q and q[0][0] == y and q[0][1] == x:
+        y, x, nc = heapq.heappop(q)
+        c += nc
+
+    ny = y + 1
+    if ny >= len(g):
+        continue
+
+    if g[ny][x] == "^":
+        r += c
+        heapq.heappush(q, (ny, x - 1, c))
+        heapq.heappush(q, (ny, x + 1, c))
+    else:
+        heapq.heappush(q, (ny, x, c))
+
+print(r)
