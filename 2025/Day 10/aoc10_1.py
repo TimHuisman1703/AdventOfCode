e6385aa6d9edf31dbc610a923797a6108b966ff9
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-11 05:40:47 +0100

    Day 10

--- 2025/Day 10/aoc10_1.py
+++ 2025/Day 10/aoc10_1.py
@@ -2,3 +2,30 @@ file = open("aoc10_input.txt")
 l = file.read().split("\n")
 file.close()
 
+import heapq
+
+r = 0
+for row in l:
+    t, *ps, _ = row.split()
+
+    t = tuple(int(v == "#") for v in t[1:-1])
+    ps = [tuple([int(j) for j in p[1:-1].split(",")]) for p in ps]
+
+    q = [(0, t)]
+    visited = set()
+    while q:
+        c, cur = heapq.heappop(q)
+
+        if not any(cur):
+            r += c
+            break
+        
+        if cur in visited:
+            continue
+        visited.add(cur)
+
+        for p in ps:
+            nxt = tuple([cur[j] ^ (j in p) for j in range(len(cur))])
+            heapq.heappush(q, (c + 1, nxt))
+
+print(r)
--- 2025/Day 10/aoc10_2.py
+++ 2025/Day 10/aoc10_2.py
@@ -2,3 +2,35 @@ file = open("aoc10_input.txt")
 l = file.read().split("\n")
 file.close()
 
+import gurobipy as gp
+
+r = 0
+for idx, row in enumerate(l):
+    _, *ps, ts = row.split()
+
+    ts = tuple(int(j) for j in ts[1:-1].split(","))
+    ps = [tuple([int(j) for j in p[1:-1].split(",")]) for p in ps]
+
+    with gp.Env(empty=True) as env:
+        env.setParam("OutputFlag", 0)
+        env.start()
+
+        with gp.Model(env=env) as model:
+            xs = [model.addVar(vtype=gp.GRB.INTEGER, name=f"x{j}") for j in range(len(ps))]
+
+            for i, x in enumerate(xs):
+                model.addConstr(x >= 0)
+
+            contains = [[] for _ in range(len(ts))]
+            for i, p in enumerate(ps):
+                for x in p:
+                    contains[x].append(i)
+            for t, con in zip(ts, contains):
+                model.addConstr(sum(xs[j] for j in con) == t)
+
+            model.setObjective(sum(xs), gp.GRB.MINIMIZE)
+            model.optimize()
+
+            r += sum(round(x.x) for x in xs)
+
+print(r)
