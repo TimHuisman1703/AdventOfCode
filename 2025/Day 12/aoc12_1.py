f2d567e9326d628e2c82018a3490963ecffe6061
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2025-12-12 12:35:27 +0100

    Day 12

--- 2025/Day 12/aoc12_1.py
+++ 2025/Day 12/aoc12_1.py
@@ -1,4 +1,21 @@
 file = open("aoc12_input.txt")
-l = file.read().split("\n")
+l = file.read()
 file.close()
 
+l = l.split("\n\n")
+ps = l[:-1]
+ps = [[[int(c == "#") for c in row] for row in p.split("\n")[1:]] for p in ps]
+
+sizes = [sum(sum(row) for row in p) for p in ps]
+
+r = 0
+for row in l[-1].split("\n"):
+    wh, *qs = row.split()
+    w, h = [int(j) for j in wh[:-1].split("x")]
+
+    qs = [int(j) for j in qs]
+
+    if sum(sizes[j] * qs[j] for j in range(len(ps))) <= w * h:
+        r += 1
+
+print(r)
